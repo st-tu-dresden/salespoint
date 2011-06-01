1549d6ced4bba3fbb1e0c497119abad69006f0fb
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-06-01 16:44:56 +0000

    Metric implemented and documented
    git-svn-id: svn://141.76.65.164/kp-salespoint@12 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/money/Metric.java
+++ SalespointFramework/src/org/salespointframework/core/money/Metric.java
@@ -2,25 +2,67 @@ package org.salespointframework.core.money;
 
 import javax.persistence.Entity;
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@DiscriminatorValue("<None>")
+@DiscriminatorValue("METRIC")
 public class Metric {
-
-	@Id
-	private long id;
+	@Id @GeneratedValue(strategy=GenerationType.AUTO) long id;
 	
 	private String name;
 	private String symbol;
 	private String definition;
 
+	/** Default class constructor is used by JPA/Hibernate. For Object creation use
+	 * parameterized public constructors instead.
+	 */
+	protected Metric() {};
+	
+	/** Class constructor specifying name, symbol and definition.
+	 * 
+	 * @param name       the name of the metric, for example 'meter'
+	 * @param symbol     symbol used to represent the metric, for example 'm'
+	 * @param definition definition of the metric, i.e. 'the base unit of length
+	 *  in the International System of Units'
+	 */
+	public Metric(String name, String symbol, String definition) {
+		this.name = name;
+		this.symbol = symbol;
+		this.definition = definition;
+	}
+	
+	/** Class constructor specifying name and symbol.
+	 * 
+	 * @param name   the name of the metric, for example 'meter'
+	 * @param symbol the symbol representing the metric, for example 'm'
+	 */
+	public Metric(String name, String symbol) {
+		this(name, symbol, "");
+	}
+	
+	/** Returns the name of this metric.
+	 * 
+	 * @return the name of this metric.
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/** Returns the symbol of this metric.
+	 * 
+	 * @return the symbol of this metric.
+	 */
 	public String getSymbol() {
 		return symbol;
 	}
+	
+	/** Returns the String containing the definition of this metric.
+	 *  The String may be empty, but is non-null.
+	 * 
+	 * @return the definition of this metric.
+	 */
 	public String getDefinition() {
 		return definition;
 	}
