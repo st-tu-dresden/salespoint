d07cde90c270be632036e87ff403ea4f6a7ec9e6
Paul Henke <paul.henke@outlook.com>
2011-06-14 08:09:28 +0000

    DELETED
    git-svn-id: svn://141.76.65.164/kp-salespoint@33 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/products/ProductInstance.java
+++
@@ -1,9 +0,0 @@
-package org.salespointframework.core.products;
-
-import org.salespointframework.core.money.Money;
-
-public interface ProductInstance<T extends ProductType> {
-	String getName();
-	Money getPrice();
-	T getProductType();
-}
--- SalespointFramework/src/org/salespointframework/core/products/ProductType.java
+++
@@ -1,9 +0,0 @@
-package org.salespointframework.core.products;
-
-import org.salespointframework.core.money.Money;
-
-public interface ProductType {
-	String getName();
-	int getProductIdentifier();
-	Money getPrice();
-}
