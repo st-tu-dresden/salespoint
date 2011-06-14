87518fcfc64de2dc3ec089ab4b6b6397c8648266
Paul Henke <paul.henke@outlook.com>
2011-06-14 08:04:55 +0000

    git-svn-id: svn://141.76.65.164/kp-salespoint@30 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/catalogs/CatalogEntry.java
+++
@@ -1,9 +0,0 @@
-package org.salespointframework.core.catalogs;
-
-import org.salespointframework.core.products.ProductType;
-
-public interface CatalogEntry<T extends ProductType> {
-	String getDescription();
-	Iterable<String> getCategories();
-	Iterable<T> getProductTypes();
-}
