518d7c720e0f0cfdc5d9a935ab6c83c930cb68a7
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-06-21 12:43:32 +0000

    Money implementation
    Money inherits from Quantity and the Metric is a currency.
    A default currency is instantiated with "Euro", although a
    user-defined currency can be supplied. The Currency class
    is no longer used, but will be kept in the repository for now.
    
    
    
    git-svn-id: svn://141.76.65.164/kp-salespoint@45 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/money/Money.java
+++ SalespointFramework/src/org/salespointframework/core/money/Money.java
@@ -1,34 +1,94 @@
 package org.salespointframework.core.money;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Entity;
 
+import org.salespointframework.core.quantity.Metric;
 import org.salespointframework.core.quantity.Quantity;
+import org.salespointframework.core.quantity.rounding.RoundDownStrategy;
 
+/** The <code>Money</code> class represents the Euro currency.
+ * A suitable <code>Metric</code> and <code>RoundingStrategy</code> is created automatically.
+ * A user-defined <code>Metric</code> can be supplied. The default <code>Metric</code> is Euro.
+ * Arithmetic functions are used from the <code>Quantity</code> class.
+ * The internal representation uses four digits after the decimal separator.
+ * 
+ * @author hannesweisbach
+ *
+ */
 @Entity
 public class Money extends Quantity {
 
-	private Currency currency;
+	{
+		metric = new Metric("Euro", "€", "");
+		roundingStrategy = new RoundDownStrategy(4);
+	}
 	
-	
-	// TODO -> protected
+	/** Parameterless constructor, do not use.
+	 * This constructor is required by the persistence layer and may not be used by application developers.
+	 */
 	@Deprecated
-	public Money() {
-		
+	protected Money() {}
+
+	/**  Creates a new <code>Money</code> instance from an amount and a metric.
+	 * 
+	 * @param amount	amount of the money represented by this instance
+	 * @param metric	metric (currency) to be used for this instance.
+	 */
+	public Money(BigDecimal amount, Metric metric) {
+		if(metric != null)
+			this.metric = metric;
+		this.amount = amount;
 	}
 	
-	@Override
-	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
-		return super.equals(obj);
+	/**  Creates a new <code>Money</code> instance from an amount and a metric.
+	 * 
+	 * @param amount	amount of the money represented by this instance
+	 * @param metric	metric (currency) to be used for this instance.
+	 */
+	public Money(int amount, Metric metric) {
+		this(BigDecimal.valueOf(amount), metric);
 	}
-
-	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return super.hashCode(); 
+	
+	/**  Creates a new <code>Money</code> instance from an amount and a metric.
+	 * 
+	 * @param amount	amount of the money represented by this instance
+	 * @param metric	metric (currency) to be used for this instance.
+	 */
+	public Money(double amount, Metric metric) {
+		this(BigDecimal.valueOf(amount), metric);
 	}
-
-	public Currency getCurrency() {
-		return currency;
+	
+	/** Constructs a new <code>Money</code> object from a <code>BigDecimal</code>.
+	 * 
+	 * @param amount the amount of money represented by <code>this</code> 
+	 */
+	public Money(BigDecimal amount) {
+		this.amount = amount;
+	}
+	
+	/** Constructs a new <code>Money</code> object from a <code>long</code> value
+	 * 
+	 * @param amount the amount of money represented by <code>this</code>
+	 */
+	public Money(long amount) {
+		this.amount = BigDecimal.valueOf(amount);
+	}
+	
+	/** Constructs a new <code>Money</code> object from a <code>double</code> value
+	 * 
+	 * @param amount the amount of money represented by <code>this</code>¸
+	 */
+	public Money(double amount) {
+		this.amount = BigDecimal.valueOf(amount);
+	}
+	
+	/** String representation of the monetary value represented by <code>this</code>.
+	 * The numerical value is truncated after two digits after the decimal separator and
+	 * followed by a whitespace and the symbol stored in <code>metric</code>.
+	 */
+	public String toString() {
+		return amount.setScale(2) + " " + metric.getSymbol();
 	}
 }
--- SalespointFramework/src/org/salespointframework/core/quantity/Quantity.java
+++ SalespointFramework/src/org/salespointframework/core/quantity/Quantity.java
@@ -17,7 +17,7 @@ import org.salespointframework.core.quantity.rounding.RoundingStrategy;
  * 'Something' is specified using a <code>Metric</code>.
  * A <code>RoundingStrategy</code> accommodates amounts
  * that do not fit the value set of a given metric 
- * (Think of money: 0.0001Û has no meaning in the real
+ * (Think of money: 0.0001ï¿½ has no meaning in the real
  * world). The <code>roundingStrategy</code> is applied
  * to <code>amount</code> in the class constructor. This
  * way, every instance has a valid <code>amount</code>.
@@ -28,9 +28,9 @@ public class Quantity implements Comparable<Quantity> {
 
 	@Id @GeneratedValue(strategy=GenerationType.AUTO) long id;
 	
-	private BigDecimal amount;
-	private Metric metric;
-	private RoundingStrategy roundingStrategy;
+	protected BigDecimal amount;
+	protected Metric metric;
+	protected RoundingStrategy roundingStrategy;
 	
 	/** Protected class constructor is required for JPA/Hibernate.
 	 * Use parameterized Constructor instead.
