f60f952455ca1cff2f9ede7f10d38d2c0d906a00
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-07-06 15:37:47 +0000

    adds test case for the OrderManager
    git-svn-id: svn://141.76.65.164/kp-salespoint@88 eb53f5e8-9ec5-4bc0-8d13-06c033283886

---
+++ SalespointFramework/test/test/order/OrderTest.java
@@ -0,0 +1,73 @@
+package test.order;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.salespointframework.core.accountancy.Accountancy;
+import org.salespointframework.core.accountancy.AccountancyEntry;
+import org.salespointframework.core.database.Database;
+import org.salespointframework.core.order.OrderEntry;
+import org.salespointframework.core.order.OrderIdentifier;
+import org.salespointframework.core.order.OrderManager;
+
+public class OrderTest {
+	private OrderManager om;
+	private List<OrderIdentifier> oi = new ArrayList<OrderIdentifier>();
+	private EntityManagerFactory emf = Database.INSTANCE.getEntityManagerFactory();
+	
+	private DateTime from;
+	private DateTime to;
+
+	@BeforeClass
+	public static void classSetup() {
+		Database.INSTANCE.initializeEntityManagerFactory("SalespointFramework");
+	}
+
+	@Before
+	public void testSetup() {
+		om = new OrderManager(emf.createEntityManager());
+		OrderEntry oe;
+		for (int year = 2000; year < 2005; year++) {
+			oe = new OrderEntry();
+			oi.add(oe.getOrderIdentifier());
+			om.addOrder(oe);
+			if(year == 2001)
+				from = new DateTime();
+			if(year == 2004)
+				to = new DateTime();
+		}
+	}
+
+	
+	public void select() {
+		Iterable<OrderEntry> i = om.findOrders(from, to);
+		
+		//TODO not really a test, because the Iterable is always non-null.
+		//Instead, we need to test for non-emptyness of the Iterable, or three
+		//elements.
+		assertNotNull(i);
+
+		for(OrderEntry e : i) {
+			System.out.println(e.toString());
+		}
+	}
+	
+	@Test
+	public void remove() {
+		OrderIdentifier e = oi.get(3);
+		System.out.println("Now:");
+		select();
+		System.out.println("removing ... " + e.toString());
+		om.remove(oi.get(3));
+		System.out.println("after:");
+		select();
+	}
+}
