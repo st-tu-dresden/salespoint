ba9beed72fadb5f95a9d3b60bdc9649e91fcb60f
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-07-12 11:17:19 +0000

    reverted to rev 69. stupid changes.
    the database field timestamp is now a java field an gets initialized
    by object construction. this allows the user to get the timestamp, which
    would otherwise be null (this is also why getTimeStamp() was stupid before).
    This was changed to allow for a consistent user experience (if you can specify
    timestamps, you should be able to read them in your results).
    
    It was previously a database field, which was initialized by default by the database,
    if an object of that type is persisted. that way, the timestamp is always correct.
    now you can hold your reference to your AccountancyEntry and wait a potentially long
    time before persisting the entry - as a result, the timestamp will be off.
    
    
    
    git-svn-id: svn://141.76.65.164/kp-salespoint@99 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/accountancy/AccountancyEntry.java
+++ SalespointFramework/src/org/salespointframework/core/accountancy/AccountancyEntry.java
@@ -6,8 +6,6 @@ import java.util.Date;
 import javax.persistence.*;
 
 import org.joda.time.DateTime;
-import org.salespointframework.core.money.Money;
-import org.salespointframework.util.Objects;
 
 /**
  * This class represents an accountancy entries. The
@@ -16,57 +14,29 @@ import org.salespointframework.util.Objects;
  * for example a <code>ProductPaymentEntry</code>.
  * 
  * @author hannesweisbach
- * @author Thomas Dedek
  * 
  */
 @Entity
 public class AccountancyEntry implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@SuppressWarnings("unused")
 	private long id;
 
 	private static final long serialVersionUID = 1L;
-	@Column(name = "TimeStamp", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false, nullable = false, updatable = false)
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date timeStamp;
 	
-	private String accountancyType;
-	private Money value;
-	private String description;
-
 	/**
 	 * Protected, parameterless Constructor required by the persistence layer.
 	 * Do not use it.
 	 */
 	@Deprecated
-	protected AccountancyEntry() {
+	public AccountancyEntry() {
+		timeStamp = new Date();
 	};
 	
-	
-	public AccountancyEntry(String accountancyType, Money value, String description) {
-		this.description = Objects.requireNonNull(description, "description");
-		this.value = Objects.requireNonNull(value, "value");
-		this.accountancyType = Objects.requireNonNull(accountancyType, "accountancyType");
-	}
-
-
-	public String getAccountancyType() {
-		return accountancyType;
-	}
-
-	public Money getValue() {
-		return value;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
 	public DateTime getTimeStamp() {
 		return new DateTime(timeStamp);
-	}
-
-	public long getId() {
-		return id;
 	}
 }
