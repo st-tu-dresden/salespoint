437c53862bff15155d219195c514ec9851cf002c
tdedek <tdedek@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-07-18 19:25:54 +0000

    EntityManager intern geregelt und OrderEntryStatus als Filterobjekt hinzugefügt. JavaDoc hinzugefügt. Der Rest des Orderpackages muss noch debugged werden und wird deswegen erst in den nächsten Tagen committed.
    git-svn-id: svn://141.76.65.164/kp-salespoint@114 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/order/OrderManager.java
+++ SalespointFramework/src/org/salespointframework/core/order/OrderManager.java
@@ -1,31 +1,58 @@
 package org.salespointframework.core.order;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.TemporalType;
 import javax.persistence.TypedQuery;
 
 import org.joda.time.DateTime;
+import org.salespointframework.core.database.Database;
 import org.salespointframework.util.Objects;
 import org.salespointframework.util.SalespointIterable;
 
-public class OrderManager {
-	private EntityManager entityManager;
 
-	public OrderManager(EntityManager entityManager) {
-		this.entityManager = Objects.requireNonNull(entityManager,
-				"entityManager");
+/**
+ * This class provides functionality to manage orders (<code>OrderEntry</code>) in the system.
+ * 
+ * @author Thomas Dedek
+ * @author hannesweisbach
+ * 
+ */
+
+public class OrderManager {
+	private EntityManagerFactory emf;
+
+	
+	/**
+	 * Create a new <code>OrderManager</code>. For persistence, an
+	 * <code>EntityManager</code> is created internally as required. The
+	 * <code>Database.INSTANCE</code> has to be initialized first.
+	 * 
+	 */
+	public OrderManager() {
+		this.emf = Database.INSTANCE.getEntityManagerFactory();
 	}
 
+	
+	/**
+	 * Add an <code>OrderEntry</code> to this
+	 * <code>OrderManager</code> and persists them to underlying database.
+	 * 
+	 * @param order The <code>OrderEntry</code> which shall be added.
+	 */
 	public void addOrder(OrderEntry order) {
-		entityManager.getTransaction().begin();
-		entityManager.persist(order);
-		entityManager.getTransaction().commit();
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(order);
+		em.getTransaction().commit();
 	}
 
 	public OrderEntry findOrder(OrderIdentifier orderIdentifier) {
 		Objects.requireNonNull(orderIdentifier, "orderIdentifier");
 
-		TypedQuery<OrderEntry> q = entityManager
+		EntityManager em = emf.createEntityManager();
+		
+		TypedQuery<OrderEntry> q = em
 				.createQuery(
 						"SELECT o FROM OrderEntry o WHERE o.orderIdentifier == :orderIdentifier",
 						OrderEntry.class);
@@ -34,11 +61,27 @@ public class OrderManager {
 		return q.getSingleResult();
 	}
 
+	
+	/**
+	 * Returns all <code>OrderEntry</code>s in between the dates
+	 * <code>from</code> and <code>to</code>, including from and to. So every
+	 * entry with an time stamp <= to and >= from is returned. If no entries
+	 * within the specified time span exist, an empty Iterable is returned.
+	 * 
+	 * @param from
+	 *            time stamp denoting the start of the requested time period
+	 * @param to
+	 *            time stamp denoting the end of the requested time period
+	 * @return an unmodifiable Iterable containing all OrderEntries between from and
+	 *         to
+	 */
 	public Iterable<OrderEntry> findOrders(DateTime from, DateTime to) {
 		Objects.requireNonNull(from, "from");
 		Objects.requireNonNull(to, "to");
 
-		TypedQuery<OrderEntry> q = entityManager
+		EntityManager em = emf.createEntityManager();
+		
+		TypedQuery<OrderEntry> q = em
 				.createQuery(
 						"SELECT o FROM OrderEntry o WHERE o.timeStamp BETWEEN :from and :to",
 						OrderEntry.class);
@@ -47,19 +90,62 @@ public class OrderManager {
 
 		return SalespointIterable.from(q.getResultList());
 	}
+	
+	
+	/**
+	 * Returns all <code>OrderEntry</code>s having the OrderEntryStatus
+	 * <code>status</code>. If no entries
+	 * with the specified status exist, an empty Iterable is returned.
+	 * 
+	 * @param status
+	 *            Denoting the OrderEntryStatus on which the OrderEntrys will be requested.
+	 * @return an unmodifiable Iterable containing all OrderEntries whith the specified OrderEntryStatus
+	 */
+	public Iterable<OrderEntry> findOrders(OrderEntryStatus status) {
+		Objects.requireNonNull(status, "status");
 
-	public void remove(OrderIdentifier orderIdentifier) {
-		Objects.requireNonNull(orderIdentifier, "orderIdentifier");
-		entityManager.getTransaction().begin();
-		entityManager.remove(entityManager.find(OrderEntry.class,
-				orderIdentifier.getIdentifier()));
-		entityManager.getTransaction().commit();
+		EntityManager em = emf.createEntityManager();
+		
+		TypedQuery<OrderEntry> q = em
+				.createQuery(
+						"SELECT o FROM OrderEntry o WHERE o.status == :status",
+						OrderEntry.class);
+		q.setParameter("status", status);
+
+		return SalespointIterable.from(q.getResultList());
 	}
 
+	/**
+	 * Remove an <code>OrderEntry</code> from this
+	 * <code>OrderManager</code> and the underlying database.
+	 * 
+	 * @param orderIdentifier The Identifier of the <code>OrderEntry</code> which shall be removed.
+	 */
+	public void remove(OrderIdentifier orderIdentifier) {
+		Objects.requireNonNull(orderIdentifier, "orderIdentifier");
+		
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.remove(em.find(OrderEntry.class,
+				orderIdentifier.getIdentifier()));
+		em.getTransaction().commit();
+	}
+
+	
+	/**
+	 * Remove an <code>OrderEntry</code> from this
+	 * <code>OrderManager</code> and the underlying database.
+	 * 
+	 * @param orderEntry The <code>OrderEntry</code> which shall be removed.
+	 */
 	public void remove(OrderEntry orderEntry) {
 		Objects.requireNonNull(orderEntry, "orderEntry");
-		entityManager.getTransaction().begin();
-		entityManager.remove(orderEntry);
-		entityManager.getTransaction().commit();
+		
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.remove(orderEntry);
+		em.getTransaction().commit();
 	}
 }
