ce53aa97d841a7073855db807148c3464f1a933c
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-07-18 12:26:31 +0000

    makes constructors public instead of protected
    git-svn-id: svn://141.76.65.164/kp-salespoint@109 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/quantity/Quantity.java
+++ SalespointFramework/src/org/salespointframework/core/quantity/Quantity.java
@@ -17,101 +17,137 @@ import org.salespointframework.core.quantity.rounding.AbstractRoundingStrategy;
 import org.salespointframework.core.quantity.rounding.RoundingStrategy;
 
 /**
+ * The Quantity class represents an amount of something. 'Something' is
+ * specified using a <code>Metric</code>. A <code>RoundingStrategy</code>
+ * accommodates amounts that do not fit the value set of a given metric (Think
+ * of money: 0.0001€ has no meaning in the real world). The
+ * <code>roundingStrategy</code> is applied to <code>amount</code> in the class
+ * constructor. This way, every instance has a valid <code>amount</code>.
  * 
  * @author hannesweisbach
  * 
- * The Quantity class represents an amount of something.
- * 'Something' is specified using a <code>Metric</code>.
- * A <code>RoundingStrategy</code> accommodates amounts
- * that do not fit the value set of a given metric 
- * (Think of money: 0.0001€ has no meaning in the real
- * world). The <code>roundingStrategy</code> is applied
- * to <code>amount</code> in the class constructor. This
- * way, every instance has a valid <code>amount</code>.
- *
  */
 @Entity
 public class Quantity implements Comparable<Quantity> {
 
-	@Id @GeneratedValue(strategy=GenerationType.AUTO) long id;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+
 	protected BigDecimal amount;
-	@ManyToOne(cascade=CascadeType.ALL, targetEntity=Metric.class)
-	//@JoinColumn(name="METRIC")
+	@OneToOne(cascade = CascadeType.ALL, targetEntity = Metric.class)
 	protected Metric metric;
-	@ManyToOne(cascade=CascadeType.ALL, targetEntity=AbstractRoundingStrategy.class)
+	@OneToOne(cascade = CascadeType.ALL, targetEntity = AbstractRoundingStrategy.class)
 	protected RoundingStrategy roundingStrategy;
-	
-	/** Protected class constructor is required for JPA/Hibernate.
-	 * Use parameterized Constructor instead.
+
+	/**
+	 * Protected class constructor is required for JPA/Hibernate. Use
+	 * parameterized Constructor instead.
 	 */
-	protected Quantity() {};
-	
-	/** Parameterized class constructor.
-	 * <code>amount</code> is immediately rounded using the supplied
-	 * <code>roundingStrategy</code>.
+	protected Quantity() {
+	};
+
+	/**
+	 * Parameterized class constructor. <code>amount</code> is immediately
+	 * rounded using the supplied <code>roundingStrategy</code>.
 	 * 
-	 * @param amount           <code>amount</code> represented by this <code>Quantity</code>
-	 * @param metric           <code>metric</code> used for this <code>Quantity</code>
-	 * @param roundingStrategy <code>roundingStrategy</code> to be used with this <code>Quantity</code>
+	 * @param amount
+	 *            <code>amount</code> represented by this <code>Quantity</code>
+	 * @param metric
+	 *            <code>metric</code> used for this <code>Quantity</code>
+	 * @param roundingStrategy
+	 *            <code>roundingStrategy</code> to be used with this
+	 *            <code>Quantity</code>
 	 */
-	protected Quantity(BigDecimal amount, Metric metric, RoundingStrategy roundingStrategy) {
+	public Quantity(BigDecimal amount, Metric metric,
+			RoundingStrategy roundingStrategy) {
 		this.amount = roundingStrategy.round(amount);
 		this.metric = metric;
 		this.roundingStrategy = roundingStrategy;
 	}
-	
-	/** Translates an <code>int</code> to a <code>Quantity</code>.
+
+	/**
+	 * Translates an <code>int</code> to a <code>Quantity</code>.
 	 * 
-	 * The Integer is rounded according to the supplied <code>RoudingStrategy</code>.
+	 * The Integer is rounded according to the supplied
+	 * <code>RoudingStrategy</code>.
 	 * 
-	 * @param amount           <code>int</code> value to be converted to <code>Quantity</code>
-	 * @param metric           <code>metric</code> to be used with this <code>Quantity</code>
-	 * @param roundingStrategy <code>roudingStrategy</code> to be used with this <code>Quantity</code>
+	 * @param amount
+	 *            <code>int</code> value to be converted to
+	 *            <code>Quantity</code>
+	 * @param metric
+	 *            <code>metric</code> to be used with this <code>Quantity</code>
+	 * @param roundingStrategy
+	 *            <code>roudingStrategy</code> to be used with this
+	 *            <code>Quantity</code>
 	 */
-	protected Quantity(int amount, Metric metric, RoundingStrategy roundingStrategy) {
+	public Quantity(int amount, Metric metric,
+			RoundingStrategy roundingStrategy) {
 		this.amount = roundingStrategy.round(new BigDecimal(amount));
 		this.metric = metric;
 		this.roundingStrategy = roundingStrategy;
 	}
-	
-	/** Translates a <code>long</code> to a <code>Quantity</code>.
+
+	/**
+	 * Translates a <code>long</code> to a <code>Quantity</code>.
 	 * 
-	 * @param amount           <code>long</code> value to be converted to a <code>Quantity</code>
-	 * @param metric           <code>metric</code> to be used with this <code>Quantity</code>
-	 * @param roundingStrategy <code>roundingStrategy</code> to be used with this <code>Quantity</code>
+	 * @param amount
+	 *            <code>long</code> value to be converted to a
+	 *            <code>Quantity</code>
+	 * @param metric
+	 *            <code>metric</code> to be used with this <code>Quantity</code>
+	 * @param roundingStrategy
+	 *            <code>roundingStrategy</code> to be used with this
+	 *            <code>Quantity</code>
 	 */
-	protected Quantity(long amount, Metric metric, RoundingStrategy roundingStrategy) {
+	public Quantity(long amount, Metric metric,
+			RoundingStrategy roundingStrategy) {
 		this.amount = roundingStrategy.round(new BigDecimal(amount));
 		this.metric = metric;
 		this.roundingStrategy = roundingStrategy;
 	}
-	
-	/** Translates a <code>float</code> to a <code>Quantity</code>.
+
+	/**
+	 * Translates a <code>float</code> to a <code>Quantity</code>.
 	 * 
-	 * @param amount           <code>float</code> value to be converted to a <code>Quantity</code>
-	 * @param metric           <code>metric</code> to be used with this <code>Quantity</code>
-	 * @param roundingStrategy <code>roundingStrategy</code> to be used with this <code>Quantity</code>
+	 * @param amount
+	 *            <code>float</code> value to be converted to a
+	 *            <code>Quantity</code>
+	 * @param metric
+	 *            <code>metric</code> to be used with this <code>Quantity</code>
+	 * @param roundingStrategy
+	 *            <code>roundingStrategy</code> to be used with this
+	 *            <code>Quantity</code>
 	 */
-	protected Quantity(float amount, Metric metric, RoundingStrategy roundingStrategy) {
+	public Quantity(float amount, Metric metric,
+			RoundingStrategy roundingStrategy) {
 		this.amount = roundingStrategy.round(new BigDecimal(amount));
 		this.metric = metric;
 		this.roundingStrategy = roundingStrategy;
 	}
-	
-	/** Translates a <code>double</code> to a <code>Quantity</code>.
+
+	/**
+	 * Translates a <code>double</code> to a <code>Quantity</code>.
 	 * 
-	 * @param amount           <code>double</code> value to be converted to a <code>Quantity</code>
-	 * @param metric           <code>metric</code> to be used with this <code>Quantity</code>
-	 * @param roundingStrategy <code>roundingStrategy</code> to be used with this <code>Quantity</code>
+	 * @param amount
+	 *            <code>double</code> value to be converted to a
+	 *            <code>Quantity</code>
+	 * @param metric
+	 *            <code>metric</code> to be used with this <code>Quantity</code>
+	 * @param roundingStrategy
+	 *            <code>roundingStrategy</code> to be used with this
+	 *            <code>Quantity</code>
 	 */
-	protected Quantity(double amount, Metric metric, RoundingStrategy roundingStrategy) {
+	public Quantity(double amount, Metric metric,
+			RoundingStrategy roundingStrategy) {
 		this.amount = roundingStrategy.round(new BigDecimal(amount));
 		this.metric = metric;
 		this.roundingStrategy = roundingStrategy;
 	}
-	
-	/** Returns the <code>metric</code> associated with this <code>Quantity</code>.
+
+	/**
+	 * Returns the <code>metric</code> associated with this
+	 * <code>Quantity</code>.
 	 * 
 	 * @return <code>Metric</code> of this <code>Quantity</code>.
 	 */
@@ -119,15 +155,17 @@ public class Quantity implements Comparable<Quantity> {
 		return metric;
 	}
 
-	/** Returns the <code>RoundingStrategy</code> of this <code>Quantity</code>.
+	/**
+	 * Returns the <code>RoundingStrategy</code> of this <code>Quantity</code>.
 	 * 
 	 * @return <code>RoundingStrategy</code> of this <code>Quantity</code>.
 	 */
 	public RoundingStrategy getRoundingStrategy() {
 		return roundingStrategy;
 	}
-	
-	/** Returns the <code>amount</code> of this as <code>float</code>.
+
+	/**
+	 * Returns the <code>amount</code> of this as <code>float</code>.
 	 * 
 	 * @return the <code>amount</code> of this as <code>float</code>.
 	 */
@@ -139,19 +177,18 @@ public class Quantity implements Comparable<Quantity> {
 	public int compareTo(Quantity o) {
 		return amount.compareTo(o.amount);
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		if(!(obj instanceof Quantity))
+		if (!(obj instanceof Quantity))
 			return false;
 		else {
-			Quantity q = (Quantity)obj;
+			Quantity q = (Quantity) obj;
 			return roundingStrategy.equals(q.roundingStrategy)
-						&& amount.equals(q.amount)
-						&& metric.equals(metric);
+					&& amount.equals(q.amount) && metric.equals(metric);
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		return amount + metric.getSymbol();
@@ -161,62 +198,75 @@ public class Quantity implements Comparable<Quantity> {
 	public int hashCode() {
 		return amount.hashCode();
 	}
-	
-	/** Sums two quantities up.
-	 * The <code>amount</code> of this and quantity are added, and a new
-	 * <code>Quantity</code>-instance is returned, representing the sum.
-	 * The metric and rounding Strategy of this are used for the new instance.
-	 * No check is performed, whether the metrics of both quantities are compatible,
-	 * i.e. it is possible to combine, say kg and s.
+
+	/**
+	 * Sums two quantities up. The <code>amount</code> of this and quantity are
+	 * added, and a new <code>Quantity</code>-instance is returned, representing
+	 * the sum. The metric and rounding Strategy of this are used for the new
+	 * instance. No check is performed, whether the metrics of both quantities
+	 * are compatible, i.e. it is possible to combine, say kg and s.
 	 * 
-	 * @param quantity <code>Quantity</code> to be added to this.
-	 * @return a new <code>Quantity</code> object representing the sum of this and <code>quantity</code>.
+	 * @param quantity
+	 *            <code>Quantity</code> to be added to this.
+	 * @return a new <code>Quantity</code> object representing the sum of this
+	 *         and <code>quantity</code>.
 	 */
 	public Quantity add(Quantity quantity) {
-		return new Quantity(amount.add(quantity.amount), metric, roundingStrategy);
+		return new Quantity(amount.add(quantity.amount), metric,
+				roundingStrategy);
 	}
-	
-	/** Subtracts a quantity from this.
-	 * The <code>amount</code> of this is subtracted by that of quantity, and a new
-	 * <code>Quantity</code>-instance is returned, representing the difference.
-	 * The metric and rounding Strategy of this are used for the new instance.
-	 * No check is performed, whether the metrics of both quantities are compatible,
-	 * i.e. it is possible to combine, say kg and s.
+
+	/**
+	 * Subtracts a quantity from this. The <code>amount</code> of this is
+	 * subtracted by that of quantity, and a new <code>Quantity</code>-instance
+	 * is returned, representing the difference. The metric and rounding
+	 * Strategy of this are used for the new instance. No check is performed,
+	 * whether the metrics of both quantities are compatible, i.e. it is
+	 * possible to combine, say kg and s.
 	 * 
-	 * @param quantity <code>Quantity</code> to be subtracted from this.
+	 * @param quantity
+	 *            <code>Quantity</code> to be subtracted from this.
 	 * @return a new <code>Quantity</code> object representing the difference of
-	 * this and <code>quantity</code>.
+	 *         this and <code>quantity</code>.
 	 */
 	public Quantity subtract(Quantity quantity) {
-		return new Quantity(amount.subtract(quantity.amount), metric, roundingStrategy);
+		return new Quantity(amount.subtract(quantity.amount), metric,
+				roundingStrategy);
 	}
-	
-	/** Multiplies two quantities.
-	 * The <code>amount</code> of this and quantity are multiplied, and a new
-	 * <code>Quantity</code>-instance is returned, representing the product.
-	 * The metric and rounding Strategy of this are used for the new instance.
-	 * No check is performed, whether the metrics of both quantities are compatible,
-	 * i.e. it is possible to combine, say kg and s.
+
+	/**
+	 * Multiplies two quantities. The <code>amount</code> of this and quantity
+	 * are multiplied, and a new <code>Quantity</code>-instance is returned,
+	 * representing the product. The metric and rounding Strategy of this are
+	 * used for the new instance. No check is performed, whether the metrics of
+	 * both quantities are compatible, i.e. it is possible to combine, say kg
+	 * and s.
 	 * 
-	 * @param quantity <code>Quantity</code> to be multiplied with this.
-	 * @return a new <code>Quantity</code> object representing the product of this and <code>quantity</code>.
+	 * @param quantity
+	 *            <code>Quantity</code> to be multiplied with this.
+	 * @return a new <code>Quantity</code> object representing the product of
+	 *         this and <code>quantity</code>.
 	 */
 	public Quantity multiply(Quantity quantity) {
-		return new Quantity(amount.multiply(quantity.amount), metric, roundingStrategy);
+		return new Quantity(amount.multiply(quantity.amount), metric,
+				roundingStrategy);
 	}
-	
-	/** Divides this by another quantity.
-	 * The <code>amount</code> of this the dividend, <code>quantity</code> the
-	 * divisor, and a new <code>Quantity</code>-instance is returned as quotient.
-	 * The metric and rounding Strategy of this are used for the new instance.
-	 * No check is performed, whether the metrics of both quantities are compatible,
-	 * i.e. it is possible to combine, say kg and s.
+
+	/**
+	 * Divides this by another quantity. The <code>amount</code> of this the
+	 * dividend, <code>quantity</code> the divisor, and a new
+	 * <code>Quantity</code>-instance is returned as quotient. The metric and
+	 * rounding Strategy of this are used for the new instance. No check is
+	 * performed, whether the metrics of both quantities are compatible, i.e. it
+	 * is possible to combine, say kg and s.
 	 * 
-	 * @param quantity <code>Quantity</code> to be used as divisor.
+	 * @param quantity
+	 *            <code>Quantity</code> to be used as divisor.
 	 * @return a new <code>Quantity</code> object representing the quotient.
 	 */
 	public Quantity divide(Quantity quantity) {
-		return new Quantity(amount.divide(quantity.amount), metric, roundingStrategy);
+		return new Quantity(amount.divide(quantity.amount), metric,
+				roundingStrategy);
 	}
 
 }
