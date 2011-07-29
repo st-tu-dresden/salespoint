9c7a18596cac3e25fc993318e6c892d863544bc9
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-07-29 11:01:24 +0000

    converts Accountancy to an interface
    * adds (final) PersistentAccountancy as implementation
    
    
    git-svn-id: svn://141.76.65.164/kp-salespoint@173 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/accountancy/Accountancy.java
+++ SalespointFramework/src/org/salespointframework/core/accountancy/Accountancy.java
@@ -1,41 +1,8 @@
 package org.salespointframework.core.accountancy;
 
-import java.io.Serializable;
-
-import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
 import org.joda.time.DateTime;
-import org.salespointframework.core.database.Database;
-import org.salespointframework.util.Objects;
-import org.salespointframework.util.SalespointIterable;
 
-/**
- * This class represents an accountancy. An accountancy consists of
- * <code>AccountancyEntries</code>.
- * 
- * @author hannesweisbach
- * @author Thomas Dedek
- * 
- */
-public class Accountancy implements Serializable {
-	private EntityManagerFactory emf;
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * Create a new <code>Accountancy</code>. For persistence, an
-	 * <code>EntityManager</code> is created internally as required. The
-	 * <code>Database.INSTANCE</code> has to be initialized first.
-	 * 
-	 * @param entityManager
-	 *            <code>EntityManager</code> which is used for this Accountancy.
-	 */
-	public Accountancy() {
-		this.emf = Database.INSTANCE.getEntityManagerFactory();
-	}
+public interface Accountancy {
 
 	/**
 	 * Adds a new <code>AccountancyEntry</code> to this <code>Accountancy</code>
@@ -47,13 +14,7 @@ public class Accountancy implements Serializable {
 	 *            <code>AccountancyEntry</code> which should be added to the
 	 *            <code>Accountancy</code>
 	 */
-	public void addEntry(AbstractAccountancyEntry accountancyEntry) {
-		EntityManager em = emf.createEntityManager();
-		Objects.requireNonNull(accountancyEntry, "accountancyEntry");
-		em.getTransaction().begin();
-		em.persist(accountancyEntry);
-		em.getTransaction().commit();
-	}
+	public void addEntry(AbstractAccountancyEntry accountancyEntry);
 
 	/**
 	 * Adds multiple <code>AccountancyEntry</code>s to this
@@ -63,14 +24,8 @@ public class Accountancy implements Serializable {
 	 * 
 	 * @param accountancyEntries
 	 */
-	public void addEntries(Iterable<AbstractAccountancyEntry> accountancyEntries) {
-		EntityManager em = emf.createEntityManager();
-		Objects.requireNonNull(accountancyEntries, "accountancyEntries");
-		em.getTransaction().begin();
-		for (AbstractAccountancyEntry e : accountancyEntries)
-			em.persist(e);
-		em.getTransaction().commit();
-	}
+	public void addEntries(
+			Iterable<AbstractAccountancyEntry> accountancyEntries);
 
 	/**
 	 * Returns all <code>AccountancyEntry</code>s in between the dates
@@ -85,21 +40,8 @@ public class Accountancy implements Serializable {
 	 * @return an unmodifiable Iterable containing all entries between from and
 	 *         to
 	 */
-	public Iterable<AbstractAccountancyEntry> getEntries(DateTime from, DateTime to) {
-		Objects.requireNonNull(from, "from");
-		Objects.requireNonNull(to, "to");
-
-		EntityManager em = emf.createEntityManager();
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<AbstractAccountancyEntry> q = cb
-				.createQuery(AbstractAccountancyEntry.class);
-		Root<AbstractAccountancyEntry> r = q.from(AbstractAccountancyEntry.class);
-		Predicate p = cb.between(r.get(AbstractAccountancyEntry_.timeStamp), from.toDate(), to.toDate());
-		q.where(p);
-		TypedQuery<AbstractAccountancyEntry> tq = em.createQuery(q);
-
-		return SalespointIterable.from(tq.getResultList());
-	}
+	public Iterable<AbstractAccountancyEntry> getEntries(
+			DateTime from, DateTime to);
 
 	/**
 	 * Returns all <code>AccountancyEntry</code>s of the specified type
@@ -110,18 +52,7 @@ public class Accountancy implements Serializable {
 	 *            The type of the entries.
 	 * @return an unmodifiable Iterable containing all entries of type clazz
 	 */
-	public <T> Iterable<T> getEntries(Class<T> clazz) {
-		Objects.requireNonNull(clazz, "clazz");
-
-		EntityManager em = emf.createEntityManager();
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<T> q = cb.createQuery(clazz);
-		Root<T> r = q.from(clazz);
-		q.where(r.type().in(clazz));
-		TypedQuery<T> tq = em.createQuery(q);
-
-		return SalespointIterable.from(tq.getResultList());
-	}
+	public <T extends AbstractAccountancyEntry> Iterable<T> getEntries(Class<T> clazz);
 
 	/**
 	 * Returns all <code>AccountancyEntry</code>s in between the dates
@@ -143,25 +74,7 @@ public class Accountancy implements Serializable {
 	 * @return an unmodifiable Iterable containing all entries between from and
 	 *         to of type T
 	 */
-	public <T extends AbstractAccountancyEntry> Iterable<T> getEntries(Class<T> clazz,
-			DateTime from, DateTime to) {
-		Objects.requireNonNull(from, "from");
-		Objects.requireNonNull(to, "to");
-		Objects.requireNonNull(clazz, "clazz");
-
-		EntityManager em = emf.createEntityManager();
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<T> q = cb.createQuery(clazz);
-		Root<T> entry = q.from(clazz);
-
-		Predicate p = entry.type().in(clazz);
-		Predicate p1 = cb.between(entry.get(AbstractAccountancyEntry_.timeStamp),
-				from.toDate(), to.toDate());
-
-		q.where(cb.and(p, p1));
-		TypedQuery<T> tq = em.createQuery(q);
-
-		return SalespointIterable.from(tq.getResultList());
-	}
+	public <T extends AbstractAccountancyEntry> Iterable<T> getEntries(
+			Class<T> clazz, DateTime from, DateTime to);
 
 }
---
+++ SalespointFramework/src/org/salespointframework/core/accountancy/PersistentAccountancy.java
@@ -0,0 +1,127 @@
+package org.salespointframework.core.accountancy;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.joda.time.DateTime;
+import org.salespointframework.core.database.Database;
+import org.salespointframework.util.Objects;
+import org.salespointframework.util.SalespointIterable;
+
+/**
+ * This class represents an accountancy. An accountancy consists of
+ * <code>AccountancyEntries</code>.
+ * 
+ * @author hannesweisbach
+ * @author Thomas Dedek
+ * 
+ */
+public final class PersistentAccountancy implements Serializable, Accountancy {
+	private EntityManagerFactory emf;
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create a new <code>Accountancy</code>. For persistence, an
+	 * <code>EntityManager</code> is created internally as required. The
+	 * <code>Database.INSTANCE</code> has to be initialized first.
+	 * 
+	 * @param entityManager
+	 *            <code>EntityManager</code> which is used for this Accountancy.
+	 */
+	public PersistentAccountancy() {
+		this.emf = Database.INSTANCE.getEntityManagerFactory();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.salespointframework.core.accountancy.IAccountancy#addEntry(org.salespointframework.core.accountancy.AbstractAccountancyEntry)
+	 */
+	@Override
+	public void addEntry(AbstractAccountancyEntry accountancyEntry) {
+		EntityManager em = emf.createEntityManager();
+		Objects.requireNonNull(accountancyEntry, "accountancyEntry");
+		em.getTransaction().begin();
+		em.persist(accountancyEntry);
+		em.getTransaction().commit();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.salespointframework.core.accountancy.IAccountancy#addEntries(java.lang.Iterable)
+	 */
+	@Override
+	public void addEntries(Iterable<AbstractAccountancyEntry> accountancyEntries) {
+		EntityManager em = emf.createEntityManager();
+		Objects.requireNonNull(accountancyEntries, "accountancyEntries");
+		em.getTransaction().begin();
+		for (AbstractAccountancyEntry e : accountancyEntries)
+			em.persist(e);
+		em.getTransaction().commit();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.salespointframework.core.accountancy.IAccountancy#getEntries(org.joda.time.DateTime, org.joda.time.DateTime)
+	 */
+	@Override
+	public Iterable<AbstractAccountancyEntry> getEntries(DateTime from, DateTime to) {
+		Objects.requireNonNull(from, "from");
+		Objects.requireNonNull(to, "to");
+
+		EntityManager em = emf.createEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<AbstractAccountancyEntry> q = cb
+				.createQuery(AbstractAccountancyEntry.class);
+		Root<AbstractAccountancyEntry> r = q.from(AbstractAccountancyEntry.class);
+		Predicate p = cb.between(r.get(AbstractAccountancyEntry_.timeStamp), from.toDate(), to.toDate());
+		q.where(p);
+		TypedQuery<AbstractAccountancyEntry> tq = em.createQuery(q);
+
+		return SalespointIterable.from(tq.getResultList());
+	}
+
+	/* (non-Javadoc)
+	 * @see org.salespointframework.core.accountancy.IAccountancy#getEntries(java.lang.Class)
+	 */
+	@Override
+	public <T extends AbstractAccountancyEntry> Iterable<T> getEntries(Class<T> clazz) {
+		Objects.requireNonNull(clazz, "clazz");
+
+		EntityManager em = emf.createEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<T> q = cb.createQuery(clazz);
+		Root<T> r = q.from(clazz);
+		q.where(r.type().in(clazz));
+		TypedQuery<T> tq = em.createQuery(q);
+
+		return SalespointIterable.from(tq.getResultList());
+	}
+
+	/* (non-Javadoc)
+	 * @see org.salespointframework.core.accountancy.IAccountancy#getEntries(java.lang.Class, org.joda.time.DateTime, org.joda.time.DateTime)
+	 */
+	@Override
+	public <T extends AbstractAccountancyEntry> Iterable<T> getEntries(Class<T> clazz,
+			DateTime from, DateTime to) {
+		Objects.requireNonNull(from, "from");
+		Objects.requireNonNull(to, "to");
+		Objects.requireNonNull(clazz, "clazz");
+
+		EntityManager em = emf.createEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<T> q = cb.createQuery(clazz);
+		Root<T> entry = q.from(clazz);
+
+		Predicate p = entry.type().in(clazz);
+		Predicate p1 = cb.between(entry.get(AbstractAccountancyEntry_.timeStamp),
+				from.toDate(), to.toDate());
+
+		q.where(cb.and(p, p1));
+		TypedQuery<T> tq = em.createQuery(q);
+
+		return SalespointIterable.from(tq.getResultList());
+	}
+
+}
