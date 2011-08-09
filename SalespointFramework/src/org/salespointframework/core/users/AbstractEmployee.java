739672f7b4dd62bf3625aa71a3b808e122892e79
hneubert <hneubert@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-08-09 13:14:05 +0000

    git-svn-id: svn://141.76.65.164/kp-salespoint@225 eb53f5e8-9ec5-4bc0-8d13-06c033283886

---
+++ SalespointFramework/src/org/salespointframework/core/users/AbstractEmployee.java
@@ -0,0 +1,43 @@
+package org.salespointframework.core.users;
+
+import javax.persistence.MappedSuperclass;
+
+import org.salespointframework.core.money.Money;
+
+/**
+ * This is an implementation of the Employee interface.
+ * @author Christopher Bellmann
+ *
+ */
+@MappedSuperclass
+public abstract class AbstractEmployee extends AbstractUser implements Employee {
+	private Money salary;
+	
+	
+	@Deprecated
+	public AbstractEmployee(){
+		super();
+	}
+	
+	
+	/**
+	 * creates a new Employee
+	 * @param userId (username) of the Employee 
+	 * @param first password of the Employee
+	 */
+	public AbstractEmployee(UserIdentifier userId, String password){
+		super(userId, password);
+	}
+	
+	@Override
+	public Money getSalary(){
+		return salary;
+	}
+	
+	@Override
+	public void changeSalary(Money salary){
+		this.salary=salary;
+	}
+
+	
+}
