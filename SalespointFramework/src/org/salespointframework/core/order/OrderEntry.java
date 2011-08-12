d59e8e1e76262ce804aa206811f34752f77b48b2
tdedek <tdedek@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-08-12 17:53:44 +0000

    Added Log functionality
    git-svn-id: svn://141.76.65.164/kp-salespoint@233 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/order/OrderEntry.java
+++ SalespointFramework/src/org/salespointframework/core/order/OrderEntry.java
@@ -1,14 +1,17 @@
 package org.salespointframework.core.order;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
@@ -53,6 +56,8 @@ public class OrderEntry {
 	private Map<String, OrderLine> orderlines = new HashMap<String, OrderLine>();
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status;
+	@ElementCollection
+	private List<OrderLogEntry> log = new ArrayList<OrderLogEntry>();
 	
 	
 	public OrderEntry() {
@@ -181,6 +186,13 @@ public class OrderEntry {
 	}
 	
 	/**
+	 * @return an Iterable with all LogEntrys from this OrderEntry
+	 */
+	public Iterable<OrderLogEntry> getLogEntries() {
+		return SalespointIterable.from(this.log);
+	}
+	
+	/**
 	 * Calculates the total price of this OrderEntry. The number of ordered objects and ChargeLines are included in the calculation.
 	 * 
 	 * @return the total price of this OrderEntry
@@ -244,8 +256,8 @@ public class OrderEntry {
 	
 	/**
 	 * Add a <code>ChargeLine</code> to this
-	 * <code>OrderEntry</code>. The ChargeLine cannot be added, 
-	 * if this OrderEntry is cancelled or closed.
+	 * <code>OrderEntry</code> with standard log entry. 
+	 * The ChargeLine cannot be added, if this OrderEntry is cancelled or closed.
 	 * 
 	 * @param chargeLine The <code>ChargeLine</code> that shall be added.
 	 */
@@ -254,12 +266,38 @@ public class OrderEntry {
 		Objects.requireNonNull(chargeLine, "chargeLine");
 		if(this.status.equals(OrderStatus.CANCELLED) || this.status.equals(OrderStatus.CLOSED)) return false;
 		
-		return this.chargeLines.add(chargeLine);
+		if(this.chargeLines.add(chargeLine)) {
+			this.log.add(new OrderLogEntry("ChargeLine: "+chargeLine.toString()+" added successfully", ""));
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Add a <code>ChargeLine</code> to this
+	 * <code>OrderEntry</code> with defined log entry. 
+	 * The ChargeLine cannot be added, if this OrderEntry is cancelled or closed.
+	 * 
+	 * @param chargeLine The <code>ChargeLine</code> that shall be added.
+	 */
+	public boolean addChargeLine(ChargeLine chargeLine, String logDescription) {
+		
+		Objects.requireNonNull(chargeLine, "chargeLine");
+		Objects.requireNonNull(logDescription, "logDescription");
+		if(this.status.equals(OrderStatus.CANCELLED) || this.status.equals(OrderStatus.CLOSED)) return false;
+		
+		if(this.chargeLines.add(chargeLine)) {
+			this.log.add(new OrderLogEntry("ChargeLine: "+chargeLine.toString()+" added successfully", logDescription));
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	/**
 	 * Remove a <code>ChargeLine</code> from this
-	 * <code>OrderEntry</code>. The ChargeLine cannot be removed, 
+	 * <code>OrderEntry</code> with standard log entry. The ChargeLine cannot be removed, 
 	 * if this OrderEntry is cancelled or closed.
 	 * 
 	 * @param id The Identifier from the <code>ChargeLine</code> that shall be removed.
@@ -281,12 +319,50 @@ public class OrderEntry {
 		}
 		
 		if(available == false) return false;
-		else return this.chargeLines.remove(lineToRemove);
+		else if(this.chargeLines.remove(lineToRemove)) {
+				this.log.add(new OrderLogEntry("ChargeLine: "+lineToRemove.toString()+" removed successfully", ""));
+				return true;
+			} else {
+				return false;
+		}
+	}
+	
+	/**
+	 * Remove a <code>ChargeLine</code> from this
+	 * <code>OrderEntry</code> with defined log entry. The ChargeLine cannot be removed, 
+	 * if this OrderEntry is cancelled or closed.
+	 * 
+	 * @param id The Identifier from the <code>ChargeLine</code> that shall be removed.
+	 */
+	public boolean removeChargeLine(OrderLineIdentifier id, String logDescription) {
+		
+		Objects.requireNonNull(id, "id");
+		Objects.requireNonNull(logDescription, "logDescription");
+		if(this.status.equals(OrderStatus.CANCELLED) || this.status.equals(OrderStatus.CLOSED)) return false;
+		
+		ChargeLine lineToRemove = null;
+		boolean available = false;
+		
+		for(ChargeLine cl : this.chargeLines) {
+			if(cl.getIdentifier().equals(id)) {
+				lineToRemove = cl;
+				available = true;
+				break;
+			}
+		}
+		
+		if(available == false) return false;
+		else if(this.chargeLines.remove(lineToRemove)) {
+				this.log.add(new OrderLogEntry("ChargeLine: "+lineToRemove.toString()+" removed successfully", logDescription));
+				return true;
+			} else {
+				return false;
+		}
 	}
 	
 	/**
 	 * Adds an <code>OrderLine</code> to this
-	 * <code>OrderEntry</code>. If the OrderLine already exists in this OrderEntry,
+	 * <code>OrderEntry</code> with standard log entry. If the OrderLine already exists in this OrderEntry,
 	 * the OrderLine will be merged with the existing OrderLine. The OrderLine cannot be added, 
 	 * if this OrderEntry is processing, cancelled or closed.
 	 * 
@@ -304,14 +380,47 @@ public class OrderEntry {
 		
 		if(orderlines.containsKey(key)) {
 			 orderlines.get(key).mergeOrderLine(o);
+			 this.log.add(new OrderLogEntry("OrderLine: "+orderLine.toString()+" merged succesfully", ""));
+		} else {
+			orderlines.put(key, o);
+			this.log.add(new OrderLogEntry("OrderLine: "+orderLine.toString()+" added succesfully", ""));
 		}
-		orderlines.put(key, o);
+		
 		return orderlines.get(key);
 	}
 	
 	/**
 	 * Adds an <code>OrderLine</code> to this
-	 * <code>OrderEntry</code>. The OrderLine cannot be added, 
+	 * <code>OrderEntry</code> with defined log entry. If the OrderLine already exists in this OrderEntry,
+	 * the OrderLine will be merged with the existing OrderLine. The OrderLine cannot be added, 
+	 * if this OrderEntry is processing, cancelled or closed.
+	 * 
+	 * @param orderLine The <code>OrderLine</code> that shall be added.
+	 */
+	public OrderLine addOrderLine(OrderLine orderLine, String logDescription) {
+		Objects.requireNonNull(orderLine, "orderLine");
+		if(this.status.equals(OrderStatus.CANCELLED) || this.status.equals(OrderStatus.CLOSED) || this.status.equals(OrderStatus.PROCESSING)) {
+			// TODO andere bessere Exception
+			throw new RuntimeException();
+		}
+		
+		String key = orderLine.getInventory().getClass().getCanonicalName();
+		OrderLine o = orderLine;
+		
+		if(orderlines.containsKey(key)) {
+			 orderlines.get(key).mergeOrderLine(o);
+			 this.log.add(new OrderLogEntry("OrderLine: "+orderLine.toString()+" merged succesfully", logDescription));
+		} else {
+			orderlines.put(key, o);
+			this.log.add(new OrderLogEntry("OrderLine: "+orderLine.toString()+" added succesfully", logDescription));
+		}
+		
+		return orderlines.get(key);
+	}
+	
+	/**
+	 * Adds an <code>OrderLine</code> to this
+	 * <code>OrderEntry</code> with standard log entry. The OrderLine cannot be added, 
 	 * if this OrderEntry is processing, cancelled or closed.
 	 * 
 	 * @param orderLine The <code>OrderLine</code> that shall be added.
@@ -330,10 +439,12 @@ public class OrderEntry {
 		
 		if(orderlines.containsKey(key)) {
 			orderlines.get(key).addSerialNumber(serialNumber);
+			this.log.add(new OrderLogEntry("OrderLine: "+orderlines.get(key).toString()+" merged succesfully", ""));
 			o = orderlines.get(key);
 		} else {
 			o = new OrderLine(inventory, serialNumber);
 			orderlines.put(key,o);
+			this.log.add(new OrderLogEntry("OrderLine: "+o.toString()+" added succesfully", ""));
 		}
 		
 		return o;
@@ -341,7 +452,39 @@ public class OrderEntry {
 	
 	/**
 	 * Adds an <code>OrderLine</code> to this
-	 * <code>OrderEntry</code>. The OrderLine cannot be added, 
+	 * <code>OrderEntry</code> with defined log entry. The OrderLine cannot be added, 
+	 * if this OrderEntry is processing, cancelled or closed.
+	 * 
+	 * @param orderLine The <code>OrderLine</code> that shall be added.
+	 */
+	public OrderLine addOrderLine(Inventory<?> inventory, SerialNumber serialNumber, String logDescription) {
+		
+		Objects.requireNonNull(inventory, "inventory");
+		Objects.requireNonNull(serialNumber, "serialNumber");
+		if(this.status.equals(OrderStatus.CANCELLED) || this.status.equals(OrderStatus.CLOSED) || this.status.equals(OrderStatus.PROCESSING)) {
+			// TODO andere bessere Exception
+			throw new RuntimeException();
+		}
+		
+		String key = inventory.getClass().getCanonicalName();
+		OrderLine o;
+		
+		if(orderlines.containsKey(key)) {
+			orderlines.get(key).addSerialNumber(serialNumber);
+			this.log.add(new OrderLogEntry("OrderLine: "+orderlines.get(key).toString()+" merged succesfully", logDescription));
+			o = orderlines.get(key);
+		} else {
+			o = new OrderLine(inventory, serialNumber);
+			orderlines.put(key,o);
+			this.log.add(new OrderLogEntry("OrderLine: "+orderlines.get(key).toString()+" added succesfully", logDescription));
+		}
+		
+		return o;
+	}
+	
+	/**
+	 * Adds an <code>OrderLine</code> to this
+	 * <code>OrderEntry</code> with standard log entry. The OrderLine cannot be added, 
 	 * if this OrderEntry is processing, cancelled or closed.
 	 * 
 	 * @param orderLine The <code>OrderLine</code> that shall be added.
@@ -361,9 +504,43 @@ public class OrderEntry {
 		if(orderlines.containsKey(key)) {
 			orderlines.get(key).addAllSerialNumbers(serialNumbers);
 			o = orderlines.get(key);
+			this.log.add(new OrderLogEntry("OrderLine: "+o.toString()+" merged succesfully", ""));
 		} else {
 			o = new OrderLine(inventory, serialNumbers);
 			orderlines.put(key,o);
+			this.log.add(new OrderLogEntry("OrderLine: "+o.toString()+" added succesfully", ""));
+		}
+		
+		return o;
+	}
+	
+	/**
+	 * Adds an <code>OrderLine</code> to this
+	 * <code>OrderEntry</code> with defined log entry. The OrderLine cannot be added, 
+	 * if this OrderEntry is processing, cancelled or closed.
+	 * 
+	 * @param orderLine The <code>OrderLine</code> that shall be added.
+	 */
+	public OrderLine addOrderLine(Inventory<?> inventory, Iterable<SerialNumber> serialNumbers, String logDescription) {
+		
+		Objects.requireNonNull(inventory, "inventory");
+		Objects.requireNonNull(serialNumbers, "serialNumbers");
+		if(this.status.equals(OrderStatus.CANCELLED) || this.status.equals(OrderStatus.CLOSED) || this.status.equals(OrderStatus.PROCESSING)) {
+			// TODO andere bessere Exception
+			throw new RuntimeException();
+		}
+		
+		String key = inventory.getClass().getCanonicalName();
+		OrderLine o;
+		
+		if(orderlines.containsKey(key)) {
+			orderlines.get(key).addAllSerialNumbers(serialNumbers);
+			o = orderlines.get(key);
+			this.log.add(new OrderLogEntry("OrderLine: "+o.toString()+" merged succesfully", logDescription));
+		} else {
+			o = new OrderLine(inventory, serialNumbers);
+			orderlines.put(key,o);
+			this.log.add(new OrderLogEntry("OrderLine: "+o.toString()+" added succesfully", logDescription));
 		}
 		
 		return o;
@@ -371,7 +548,7 @@ public class OrderEntry {
 	
 	/**
 	 * Remove an <code>OrderLine</code> from this
-	 * <code>OrderEntry</code>. The OrderLine cannot be removed, 
+	 * <code>OrderEntry</code> with standard log entry. The OrderLine cannot be removed, 
 	 * if this OrderEntry is processing, cancelled or closed.
 	 * 
 	 * @param id The Identifier from the <code>OrderLine</code> that shall be removed.
@@ -395,12 +572,57 @@ public class OrderEntry {
 		}
 		
 		if(available == false) return null;
-		else return this.orderlines.remove(lineToRemove.getInventory().getClass().getCanonicalName());
+		else {
+			OrderLine ret = this.orderlines.remove(lineToRemove.getInventory().getClass().getCanonicalName());
+			
+			if(ret != null) {
+				this.log.add(new OrderLogEntry("OrderLine: "+ret.toString()+" removed succesfully", ""));
+			}
+			
+			return ret;
+		}
+	}
+	
+	/**
+	 * Remove an <code>OrderLine</code> from this
+	 * <code>OrderEntry</code> with defined log entry. The OrderLine cannot be removed, 
+	 * if this OrderEntry is processing, cancelled or closed.
+	 * 
+	 * @param id The Identifier from the <code>OrderLine</code> that shall be removed.
+	 * 
+	 * @return the OrderLine that was removed, or null if nothing is removed.
+	 */
+	public OrderLine removeOrderLine(OrderLineIdentifier id, String logDescription) {
+		
+		Objects.requireNonNull(id, "id");
+		if(this.status.equals(OrderStatus.CANCELLED) || this.status.equals(OrderStatus.CLOSED) || this.status.equals(OrderStatus.PROCESSING)) return null;
+		
+		OrderLine lineToRemove = null;
+		boolean available = false;
+		
+		for(OrderLine ol : this.orderlines.values()) {
+			if(ol.getIdentifier().equals(id)) {
+				lineToRemove = ol;
+				available = true;
+				break;
+			}
+		}
+		
+		if(available == false) return null;
+		else {
+			OrderLine ret = this.orderlines.remove(lineToRemove.getInventory().getClass().getCanonicalName());
+			
+			if(ret != null) {
+				this.log.add(new OrderLogEntry("OrderLine: "+ret.toString()+" removed succesfully", logDescription));
+			}
+			
+			return ret;
+		}
 	}
 	
 	/**
 	 * Change the <code>OrderStatus</code> from this
-	 * <code>OrderEntry</code>.
+	 * <code>OrderEntry</code>. Standard log entry is created.
 	 * 
 	 * @param status The OrderStatus to which the <code>OrderEntry</code> shall be changed.
 	 */
@@ -408,6 +630,63 @@ public class OrderEntry {
 		
 		Objects.requireNonNull(orderStatus, "orderStatus");
 		this.status = orderStatus;
+		this.log.add(new OrderLogEntry("OrderStatus switched to "+orderStatus.toString(), ""));
+		
+		switch (orderStatus)
+        {
+          case CANCELLED:
+        	  
+        	  for(OrderLine ol : this.orderlines.values()) {
+        		  ol.mutableChargeLines = false;
+        		  ol.mutableOrderLine = false;
+        	  }
+        	  break;
+        	  
+          case CLOSED:
+        	  
+        	  for(OrderLine ol : this.orderlines.values()) {
+        		  ol.mutableChargeLines = false;
+        		  ol.mutableOrderLine = false;
+        	  }
+        	  break;
+        	  
+          case OPEN:
+        	  
+        	  for(OrderLine ol : this.orderlines.values()) {
+        		  ol.mutableChargeLines = true;
+        		  ol.mutableOrderLine = true;
+        	  }
+        	  break;
+        	  
+          case INITIALIZED:
+        	  
+        	  for(OrderLine ol : this.orderlines.values()) {
+        		  ol.mutableChargeLines = true;
+        		  ol.mutableOrderLine = true;
+        	  }
+        	  break;
+        	  
+          case PROCESSING:
+        	  
+        	  for(OrderLine ol : this.orderlines.values()) {
+        		  ol.mutableChargeLines = true;
+        		  ol.mutableOrderLine = false;
+        	  }
+        	  break;
+        }
+	}
+	
+	/**
+	 * Change the <code>OrderStatus</code> from this
+	 * <code>OrderEntry</code>. A log entry with the given description is created.
+	 * 
+	 * @param status The OrderStatus to which the <code>OrderEntry</code> shall be changed.
+	 */
+	public void changeOrderStatus(OrderStatus orderStatus, String logDescription) {
+		
+		Objects.requireNonNull(orderStatus, "orderStatus");
+		this.status = orderStatus;
+		this.log.add(new OrderLogEntry("OrderStatus switched to "+orderStatus.toString(), logDescription));
 		
 		switch (orderStatus)
         {
