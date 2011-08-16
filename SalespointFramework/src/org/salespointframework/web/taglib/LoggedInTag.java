b424eb151a9ed01ebc4acc13df9feaeed8279488
Paul Henke <paul.henke@outlook.com>
2011-08-16 00:47:15 +0000

    jetzt bestimmt schon das SVN Designentscheidungen, .tags wollte es nicht, also .taglib
    git-svn-id: svn://141.76.65.164/kp-salespoint@253 eb53f5e8-9ec5-4bc0-8d13-06c033283886

---
+++ SalespointFramework/src/org/salespointframework/web/taglib/LoggedInTag.java
@@ -0,0 +1,14 @@
+package org.salespointframework.web.taglib;
+
+import org.springframework.web.servlet.tags.RequestContextAwareTag;
+
+public class LoggedInTag  extends RequestContextAwareTag{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected int doStartTagInternal() throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ SalespointFramework/src/org/salespointframework/web/taglib/LoginTag.java
@@ -0,0 +1,13 @@
+package org.salespointframework.web.taglib;
+
+import org.springframework.web.servlet.tags.RequestContextAwareTag;
+
+public class LoginTag extends RequestContextAwareTag {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected int doStartTagInternal() throws Exception {
+		return 0;
+	}
+
+}
