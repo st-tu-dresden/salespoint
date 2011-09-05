a1d1f47627a04110a77e4785ded167706e6995fe
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-09-05 19:31:48 +0000

    adds requested Accountancy features
    methods to query for entries with time spec.
    spec include from, to and period.
    the time between from and to is divided into fragments of time
    of period length. for each fragment, 
    a) an iterable is returned, containing all entries within that fragment
    b) a money object is returned representing the sum of the value of all
    entries within that fragment.
    
    this allows you to easily create charts etc; for example
    plot monthly salesvolume over the last two years:
    a.getSalesVolume(ProductPaymentEntry.class, new DateTime().minusYears(2), new DateTime(), Period.months(1));
    this will return an Iterable<Interval, Money> containing the sales volume in Money
    for every month, specified in Interval.
    
    
    
    git-svn-id: svn://141.76.65.164/kp-salespoint@326 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/accountancy/Accountancy.java
+++ SalespointFramework/src/org/salespointframework/core/accountancy/Accountancy.java
@@ -1,6 +1,11 @@
 package org.salespointframework.core.accountancy;
 
+import java.util.Map;
+
 import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.Period;
+import org.salespointframework.core.money.Money;
 
 public interface Accountancy {
 
@@ -24,7 +29,7 @@ public interface Accountancy {
 	 * 
 	 * @param accountancyEntries
 	 */
-	 void addEntries(Iterable<AbstractAccountancyEntry> accountancyEntries);
+	void addEntries(Iterable<AbstractAccountancyEntry> accountancyEntries);
 
 	/**
 	 * Returns all <code>AccountancyEntry</code>s in between the dates
@@ -41,8 +46,6 @@ public interface Accountancy {
 	 */
 	Iterable<AbstractAccountancyEntry> getEntries(DateTime from, DateTime to);
 
-	
-	
 	// TODO häßlich mit Class<T>, gez. Paul
 	/**
 	 * Returns all <code>AccountancyEntry</code>s of the specified type
@@ -75,6 +78,75 @@ public interface Accountancy {
 	 * @return an unmodifiable Iterable containing all entries between from and
 	 *         to of type T
 	 */
-	<T extends AbstractAccountancyEntry> Iterable<T> getEntries(Class<T> clazz, DateTime from, DateTime to);
+	<T extends AbstractAccountancyEntry> Iterable<T> getEntries(Class<T> clazz,
+			DateTime from, DateTime to);
 
+	/**
+	 * Returns all <code>AccountancyEntry</code>s of type <code>clazz</code>,
+	 * which have their <code>timeStamp</code> within (including)
+	 * <code>from</code> and <code>to</code>. <br>
+	 * The time between <code>from</code> and <code>to</code> is divided into
+	 * parts of <code>period</code> length. According to their time stamp,
+	 * entries are sorted in exactly one of the time intervals. The last time
+	 * interval may be shorter than <code>period</code>.<br>
+	 * Returned is a map, having a <code>Interval</code> objects as its key, and
+	 * an <code>Iterable&lt;T&gt;</code> as value. The <code>Iterable</code>
+	 * contains all entries of the specific type with its time stamp in the
+	 * interval specified by the key.
+	 * 
+	 * 
+	 * @param clazz
+	 *            class type of the requested entries, has to be a subclass of
+	 *            AbstractAccountancyEntry
+	 * @param from
+	 *            all returned entries will have a time stamp after
+	 *            <code>from</code>
+	 * @param to
+	 *            all returned entries will have a time stamp before
+	 *            <code>to</code>
+	 * @param period
+	 *            length of the time intervals, the period between
+	 *            <code>from</code> and <code>to</code> is divided
+	 * @return a map, with intervals of <code>period</code> length between
+	 *         <code>from</code> and <code>to</code> as keys, and as value an
+	 *         <code>Iterable</code> containing all entries within the key-
+	 *         <code>Interval</code>
+	 */
+	<T extends AbstractAccountancyEntry> Map<Interval, Iterable<T>> getEntries(
+			Class<T> clazz, DateTime from, DateTime to, Period period);
+
+	/**
+	 * Returns the sum of the field <code>amount</code> of all
+	 * <code>AccountancyEntry</code>s of type <code>clazz</code>, which have
+	 * their <code>timeStamp</code> within (including) <code>from</code> and
+	 * <code>to</code>. <br>
+	 * The time between <code>from</code> and <code>to</code> is divided into
+	 * parts of <code>period</code> length. According to their time stamp,
+	 * entries are sorted in exactly one of the time intervals. The last time
+	 * interval may be shorter than <code>period</code>.<br>
+	 * Returned is a map, having a <code>Interval</code> objects as its key, and
+	 * an <code>Money</code> as value. The <code>Money</code> object's value is
+	 * equal to the sum of all entries' <code>amount</code>-field, with a time
+	 * stamp within the key-<code>Interval</code>.
+	 * 
+	 * 
+	 * @param clazz
+	 *            class type of the requested entries, has to be a subclass of
+	 *            AbstractAccountancyEntry
+	 * @param from
+	 *            all returned entries will have a time stamp after
+	 *            <code>from</code>
+	 * @param to
+	 *            all returned entries will have a time stamp before
+	 *            <code>to</code>
+	 * @param period
+	 *            length of the time intervals, the period between
+	 *            <code>from</code> and <code>to</code> is divided
+	 * @return a map, with intervals of <code>period</code> length between
+	 *         <code>from</code> and <code>to</code> as keys, and as value a
+	 *         <code>Money</code> object, equal to the sum of the amount fields
+	 *         of all entries within the key-<code>Interval</code>
+	 */
+	<T extends AbstractAccountancyEntry> Map<Interval, Money> getSalesVolume(
+			Class<T> clazz, DateTime from, DateTime to, Period period);
 }
--- SalespointFramework/src/org/salespointframework/core/accountancy/PersistentAccountancy.java
+++ SalespointFramework/src/org/salespointframework/core/accountancy/PersistentAccountancy.java
@@ -1,6 +1,9 @@
 package org.salespointframework.core.accountancy;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -9,7 +12,10 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.Period;
 import org.salespointframework.core.database.Database;
+import org.salespointframework.core.money.Money;
 import org.salespointframework.util.Iterables;
 import org.salespointframework.util.Objects;
 
@@ -120,8 +126,44 @@ public final class PersistentAccountancy implements Serializable, Accountancy {
 
 		q.where(cb.and(p, p1));
 		TypedQuery<T> tq = em.createQuery(q);
-
+		
 		return Iterables.from(tq.getResultList());
+	}
+
+	@Override
+	public <T extends AbstractAccountancyEntry> Map<Interval, Iterable<T>> getEntries(
+			Class<T> clazz, DateTime from, DateTime to, Period period) {
+		DateTime nextStep;
+		HashMap<Interval, Iterable<T>> entries = new HashMap<Interval, Iterable<T>>();
+		
+		for(; from.isBefore(to.minus(period)); from = from.plus(period)) {
+			nextStep = from.plus(period);
+			entries.put(new Interval(from, nextStep), getEntries(clazz, from, nextStep));
+		}
+		/* Remove last interval from loop, to save the test for the last interval in every
+		 * iteration. But it's java, it not like you're gonna notice the speedup, hahaha.
+		 * BTW: the cake is a lie, hahaha.
+		 */
+		entries.put(new Interval(from, to), getEntries(clazz, from, to));
+		return entries;
+	}
+
+	@Override
+	public <T extends AbstractAccountancyEntry> Map<Interval, Money> getSalesVolume(
+			Class<T> clazz, DateTime from, DateTime to, Period period) {
+		Money total;
+		Map<Interval, Money> sales = new HashMap<Interval, Money>();
+		Map<Interval, Iterable<T>> entries = getEntries(clazz, from, to, period);
+		
+		for(Entry<Interval, Iterable<T>> e : entries.entrySet()) {
+			total = Money.ZERO;
+			for(T t : e.getValue()) {
+				total = total.add_(t.getValue());
+			}
+			sales.put(e.getKey(), total);
+		}
+	
+		return sales;
 	}
 
 }
