cb0c93451618dc6cd10a37efd0b7eaa0af95ab3f
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-09-08 16:10:37 +0000

    fixes javadoc typos
    git-svn-id: svn://141.76.65.164/kp-salespoint@347 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/users/Employee.java
+++ SalespointFramework/src/org/salespointframework/core/users/Employee.java
@@ -3,7 +3,7 @@ package org.salespointframework.core.users;
 import org.salespointframework.core.money.Money;
 
 /**
- * An Employee is a spezial User. The Employee should have a salary.
+ * An Employee is a special user. The employee has a salary.
  * This salary can be used in the Accountancy.
  * @author Christopher Bellmann
  *
@@ -11,7 +11,7 @@ import org.salespointframework.core.money.Money;
 public interface Employee extends User{
 	
 	/**
-	 * Set the salary of the Employee to the given parameter
+	 * Set the salary of the employee to the given parameter
 	 * @param salary new salary
 	 */
 	public void changeSalary(Money salary);
