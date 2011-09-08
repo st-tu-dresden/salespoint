df89515c8ac40c921e61757e5a86eac2a2f1af94
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-09-08 16:00:23 +0000

    fix/add javadoc in interface User
    git-svn-id: svn://141.76.65.164/kp-salespoint@344 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/users/User.java
+++ SalespointFramework/src/org/salespointframework/core/users/User.java
@@ -1,25 +1,40 @@
 package org.salespointframework.core.users;
 
 /**
- * This the User Interface. A User should have an username and a password.
+ * This interface is the base interface, implemented by all user classes.
+ * 
  * @author Christopher Bellmann
- *
+ * 
  */
 public interface User {
-	
+
 	/**
-	 * Verfies if the given password is equal with the user's password.
-	 * @param password you want to check
-	 * @return true if password is correct
+	 * Checks a given password against the user's password.
+	 * 
+	 * @param password
+	 *            The password to be checked.
+	 * @return <code>true</code>, if the password matches, <code>false</code>
+	 *         otherwise.
 	 */
 	public boolean verifyPassword(String password);
-	
-		
+
 	/**
+	 * Get the unique identifier of this <code>User</code>.
 	 * 
-	 * @return the users name/ID
+	 * @return the <code>UserIdentifier</code> of this <code>User</code>
 	 */
 	public UserIdentifier getUserIdentifier();
 
+	/**
+	 * Changes the password of the <code>User</code> to <code>newPassword</code>
+	 * . Before the change is made, <code>oldPassword</code> is checked against
+	 * the current password. If the check is successful, the password is
+	 * changed.
+	 * 
+	 * @param newPassword
+	 *            new password.
+	 * @param oldPassword
+	 *            old password, which is checked against the current password.
+	 */
+	public boolean changePassword(String newPassword, String oldPassword);
 }
-
