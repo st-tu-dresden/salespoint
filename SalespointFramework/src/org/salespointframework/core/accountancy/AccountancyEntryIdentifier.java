71fc2dc94a6372ccd7defeac96ff807f019cf3e5
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-09-18 13:53:55 +0000

    removes explicit constructors from AccountancyEntryIdentifier adds javadoc to AccountancyEntryIdentifier
    if you re-add explicit constructors, add comments as to why you do so,
    and javadoc them properly.
    
    
    git-svn-id: svn://141.76.65.164/kp-salespoint@415 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/accountancy/AccountancyEntryIdentifier.java
+++ SalespointFramework/src/org/salespointframework/core/accountancy/AccountancyEntryIdentifier.java
@@ -4,17 +4,18 @@ import javax.persistence.Embeddable;
 
 import org.salespointframework.util.SalespointIdentifier;
 
+/**
+ * <code>AccountancyEntryIdentifier</code> serves as an identifier type for
+ * <code>AccountancyEntry</code> objects. The main reason for its existence is
+ * typesafety for identifier across the Salespoint Framework. <br>
+ * <code>AccoutancyEntryIdentifier</code> instances serve as primary key
+ * attribute in <code>PersistentAccountancyEntry</code>, but can also be used as
+ * a key for non-persistent, <code>Map</code>-based implementations.
+ * 
+ * @author hannesweisbach
+ * 
+ */
 @SuppressWarnings("serial")
 @Embeddable
-public class AccountancyEntryIdentifier extends SalespointIdentifier
-{
-	public AccountancyEntryIdentifier()
-	{
-		super();
-	}
-
-	public AccountancyEntryIdentifier(String accountancyEntryIdentifier)
-	{
-		super(accountancyEntryIdentifier);
-	}
+public class AccountancyEntryIdentifier extends SalespointIdentifier {
 }
