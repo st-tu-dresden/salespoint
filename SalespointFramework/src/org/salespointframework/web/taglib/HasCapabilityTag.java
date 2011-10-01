de0857ce139d8c3fba9c44883ba7d7fe75887e6c
tdedek <tdedek@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-10-01 12:54:47 +0000

    UserCapability reference fix
    git-svn-id: svn://141.76.65.164/kp-salespoint@472 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/web/taglib/HasCapabilityTag.java
+++ SalespointFramework/src/org/salespointframework/web/taglib/HasCapabilityTag.java
@@ -5,7 +5,7 @@ import javax.servlet.jsp.tagext.BodyTagSupport;
 
 import org.salespointframework.core.shop.Shop;
 import org.salespointframework.core.user.User;
-import org.salespointframework.core.user.Capability;
+import org.salespointframework.core.user.UserCapability;
 import org.salespointframework.core.user.UserManager;
 
 /**
@@ -30,7 +30,7 @@ public class HasCapabilityTag extends BodyTagSupport
 		@SuppressWarnings("unchecked")
 		UserManager<User> usermanager = (UserManager<User>) Shop.INSTANCE.getUserManager();
 		User user = usermanager.getUserByToken(User.class, pageContext.getSession());
-		Capability capability = new Capability(capabilityName);
+		UserCapability capability = new UserCapability(capabilityName);
 
 		if (user != null)
 		{
