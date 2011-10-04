f60583765506406c67f2db7dba4f3b4dd0f454a7
sfoerster <sfoerster@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-10-04 13:13:45 +0000

    - fixed javadoc of UserIdentifier
    git-svn-id: svn://141.76.65.164/kp-salespoint@517 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/user/UserIdentifier.java
+++ SalespointFramework/src/org/salespointframework/core/user/UserIdentifier.java
@@ -5,33 +5,33 @@ import javax.persistence.Embeddable;
 import org.salespointframework.util.SalespointIdentifier;
 
 /**
- * <code>UserIdentifier</code> serves as an identifier type for
- * {@link User} objects. The main reason for its existence is
- * type safety for identifier across the Salespoint Framework. <br>
- * <code>UserIdentifier</code> instances serve as primary key
- * attribute in {@link PersistentUser}, but can also be used as
- * a key for non-persistent, <code>Map</code>-based implementations.
+ * <code>UserIdentifier</code> serves as an identifier type for {@link User}
+ * objects. The main reason for its existence is type safety for identifier
+ * across the Salespoint Framework. <br>
+ * <code>UserIdentifier</code> instances serve as primary key attribute in
+ * {@link PersistentUser}, but can also be used as a key for non-persistent,
+ * <code>Map</code>-based implementations.
  * 
  * @author Hannes Weisbach
  */
 @SuppressWarnings("serial")
 @Embeddable
-public final class UserIdentifier extends SalespointIdentifier
-{
-	/**
-	 * Creates a new unique identifier for {@link User}s.
-	 */
-	public UserIdentifier()
-	{
-		super();
-	}
+public final class UserIdentifier extends SalespointIdentifier {
+    /**
+     * Creates a new unique identifier for {@link User}s.
+     */
+    public UserIdentifier() {
+        super();
+    }
 
-	/**
-	 * TODO
-	 * @param userIdentifier
-	 */
-	public UserIdentifier(String userIdentifier)
-	{
-		super(userIdentifier);
-	}
+    /**
+     * Creates a new identifier for {@link User}s. This self defined identifier
+     * is not guaranteed to be unique. But you can provide a human readable
+     * value.
+     * 
+     * @param userIdentifier The value of this identifier. Will not be checked to be unique.
+     */
+    public UserIdentifier(String userIdentifier) {
+        super(userIdentifier);
+    }
 }
