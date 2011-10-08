8c6c23f6a3e9e04c127cfbf9bcda972cc7d33f24
Paul Henke <paul.henke@outlook.com>
2011-10-08 14:19:16 +0000

    mit der Klasse sollte man den static ctor, welcher die DB initialisiert, aus den @Controller werfen können noch ungetestet
    git-svn-id: svn://141.76.65.164/kp-salespoint@646 eb53f5e8-9ec5-4bc0-8d13-06c033283886

---
+++ SalespointFramework/src/org/salespointframework/web/PersistenceUnitInitializer.java
@@ -0,0 +1,22 @@
+package org.salespointframework.web;
+
+import org.salespointframework.core.database.Database;
+
+
+/**
+ * @author Paul Henke
+ *
+ */
+public class PersistenceUnitInitializer {
+
+	private String persistenceUnitName;
+	
+	public PersistenceUnitInitializer() {
+		
+	}
+	
+	public void setPersistenceUnitName(String persistenceUnitName) {
+		this.persistenceUnitName = persistenceUnitName;
+		Database.INSTANCE.initializeEntityManagerFactory(persistenceUnitName);
+	}
+}
