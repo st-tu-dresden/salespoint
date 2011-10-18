e52c9b2dff5dd0bd8c83cc8f319c6d2d7a928d6a
sfoerster <sfoerster@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2011-10-18 17:44:51 +0000

    simplified week number calculation in CallendarController
    git-svn-id: svn://141.76.65.164/kp-salespoint@737 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- Calendar/src/calendar/controller/CalendarController.java
+++ Calendar/src/calendar/controller/CalendarController.java
@@ -8,8 +8,6 @@ import java.util.Map;
 import javax.servlet.http.HttpSession;
 
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeConstants;
-import org.joda.time.Period;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.DateTimeFormatterBuilder;
 import org.salespointframework.core.calendar.CalendarEntryCapability;
@@ -33,48 +31,28 @@ public class CalendarController {
     private static final DateTimeFormatter inputFormatter = new DateTimeFormatterBuilder().appendYear(4, 4).appendLiteral("-").appendMonthOfYear(2)
                                                                           .appendLiteral("-").appendDayOfMonth(2).appendLiteral("T").appendHourOfDay(2)
                                                                           .appendLiteral(":").appendMinuteOfHour(2).toFormatter();
-    private static final Period            ONE_MONTH      = new Period(0, 1, 0, 0, 0, 0, 0, 0);
-    private static final Period            ONE_DAY        = new Period(0, 0, 0, 1, 0, 0, 0, 0);
 
     private ModelAndView addMonthData(int year, int month, ModelAndView mav) {
         DateTime firstDayOfMonth = new DateTime(year, month, 1, 0, 0, 0, 0);
-        int week = 0;
 
-        // last week is needed for December
-        // if first or last day of a year is a Thursday, the year has 53 week,
-        // otherwise there are 52 weeks per year.
-        int numberOfWeeksLastYear = (new DateTime(year - 1, 1, 1, 0, 0, 0, 0).getDayOfWeek() == DateTimeConstants.THURSDAY || new DateTime(year - 1, 12, 31, 0,
-                        0, 0, 0).getDayOfWeek() == DateTimeConstants.THURSDAY) ? 53 : 52;
-        int numberOfWeeksThisYear = (new DateTime(year, 1, 1, 0, 0, 0, 0).getDayOfWeek() == DateTimeConstants.THURSDAY || new DateTime(year, 12, 31, 0, 0, 0, 0)
-                        .getDayOfWeek() == DateTimeConstants.THURSDAY) ? 53 : 52;
-
-        if (month != DateTimeConstants.JANUARY) {
-            week = firstDayOfMonth.weekOfWeekyear().get();
-        } else {
-            if (firstDayOfMonth.getDayOfWeek() < DateTimeConstants.FRIDAY)
-                week = 1;
-            else
-                week = numberOfWeeksLastYear;
-        }
-
+        String monthString = monthFormatter.print(firstDayOfMonth);
+        int firstWeekdayOfMonth = firstDayOfMonth.getDayOfWeek();
+        int numberOfDaysPerMonth = firstDayOfMonth.plusMonths(1).minusDays(1).getDayOfMonth();
+        
         int[] weekNumbers = new int[6];
         for (int i = 0; i < 6; i++) {
-
-            if ((month == DateTimeConstants.JANUARY && week > numberOfWeeksLastYear) || (month == DateTimeConstants.DECEMBER && week > numberOfWeeksThisYear))
-                week = 1;
-
-            weekNumbers[i] = week++;
+            weekNumbers[i] = firstDayOfMonth.getWeekOfWeekyear();
+            firstDayOfMonth = firstDayOfMonth.plusWeeks(1);
         }
 
-        int firstWeekdayOfMonth = firstDayOfMonth.getDayOfWeek();
-        int numberOfDaysPerMonth = firstDayOfMonth.plus(ONE_MONTH).minus(ONE_DAY).getDayOfMonth();
+        
 
         mav.addObject("firstWeekday", Integer.valueOf(firstWeekdayOfMonth));
         mav.addObject("numberOfDays", Integer.valueOf(numberOfDaysPerMonth));
         mav.addObject("weekNumbers", weekNumbers);
         mav.addObject("year", Integer.valueOf(year));
-        mav.addObject("month", Integer.valueOf(firstDayOfMonth.getMonthOfYear()));
-        mav.addObject("monthTxt", monthFormatter.print(firstDayOfMonth));
+        mav.addObject("month", Integer.valueOf(month));
+        mav.addObject("monthTxt", monthString);
 
         return mav;
     }
