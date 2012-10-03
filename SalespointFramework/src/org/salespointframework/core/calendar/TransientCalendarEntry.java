84aea966bc8a87f71c3fd753a42522b83f8b2974
Paul Henke <paul.henke@outlook.com>
2012-10-03 18:56:27 +0000

    transient calendar
    git-svn-id: svn://141.76.65.164/kp-salespoint@853 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/calendar/TransientCalendarEntry.java
+++ SalespointFramework/src/org/salespointframework/core/calendar/TransientCalendarEntry.java
@@ -1,79 +1,167 @@
 package org.salespointframework.core.calendar;
 
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
 import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.Period;
 import org.salespointframework.core.user.UserIdentifier;
 
 public class TransientCalendarEntry implements CalendarEntry
 {
+	
+	// TODO Persistent angucken, mehr checkano!!!
+	
+	
+	
+	private final CalendarEntryIdentifier calendarEntryIdentifier = new CalendarEntryIdentifier();
+
+	private int repeatCount;
+
+	private String description;
+
+	private Interval duration;
+
+	private DateTime start;
+
+	private DateTime end;
+
+	private UserIdentifier owner;
+
+	private Period period;
+
+	private String title;
+	
+    public TransientCalendarEntry(UserIdentifier owner, String title, DateTime start, DateTime end) {
+        this(owner, title, start, end, "", Period.ZERO, 0);
+    }
+    
+    public TransientCalendarEntry(UserIdentifier owner, String title, DateTime start, DateTime end, String description) {
+        this(owner, title, start, end, description, Period.ZERO, 0);
+    }
+    
+    public TransientCalendarEntry(UserIdentifier owner, String title, DateTime start, DateTime end, String description, Period period, int count) {
+
+        detectDateAnomalies(Objects.requireNonNull(start, "start must not be null"), Objects.requireNonNull(end, "end must not be null"), count, Objects.requireNonNull(period, "period must not be null"));
+
+        this.owner = Objects.requireNonNull(owner, "owner must not be null must not be null");
+        this.title = Objects.requireNonNull(title, "title must not be null");
+        this.description = Objects.requireNonNull(description, "description must not be null");
+
+        this.repeatCount = count;
+        this.period = period;
+
+        this.start = start;
+        this.end = end;
+        
+        duration = new Interval(start, end);
+    }
 
 	@Override
 	public CalendarEntryIdentifier getIdentifier()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return calendarEntryIdentifier;
 	}
 
 	@Override
 	public String getDescription()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return description;
 	}
 
 	@Override
 	public DateTime getEnd()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return end;
 	}
 
 	@Override
 	public UserIdentifier getOwner()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return owner;
 	}
 
 	@Override
 	public DateTime getStart()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return start;
 	}
 
 	@Override
 	public String getTitle()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return title;
 	}
 
 	@Override
 	public void setDescription(String description)
 	{
-		// TODO Auto-generated method stub
+		this.description = Objects.requireNonNull(description, "description must not be null");
 
 	}
 
 	@Override
 	public void setEnd(DateTime end)
 	{
-		// TODO Auto-generated method stub
+		this.end = Objects.requireNonNull(end, "end must not be null");
 
 	}
 
 	@Override
 	public void setStart(DateTime start)
 	{
-		// TODO Auto-generated method stub
+		this.start = Objects.requireNonNull(start, "start must not be null");
 
 	}
 
 	@Override
 	public void setTitle(String title)
 	{
-		// TODO Auto-generated method stub
+		this.title = Objects.requireNonNull(title, "title must not be null");
 
 	}
+	
+    private final void detectDateAnomalies(DateTime start, DateTime end, int repeatCount, Period period) {
+        Interval duration = new Interval(start, end);
+        
+        if (repeatCount < -1)
+            throw new IllegalArgumentException("The repeat count of an appointment can't be less than zero, except -1, but was " + repeatCount);
+
+        if ((repeatCount != 0) && duration.getStart().plus(period).isBefore(duration.getEnd()))
+            throw new IllegalArgumentException("The time between two repetitions can not be less than the duration of a calendar entry!");
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null)
+            return false;
+        if (other == this)
+            return true;
+        if (other instanceof TransientCalendarEntry)
+            return this.calendarEntryIdentifier.equals(((TransientCalendarEntry)other).calendarEntryIdentifier);
+        return false;
+    }
+    
+    /**
+     * Returns a string representation of this entry. The format of this string
+     * is:
+     * <p>
+     * <code>title + " (" + getStart() + " - " + getEnd() + ")"</code>
+     * </p>
+     * 
+     * @return The string representation of this entry.
+     */
+    @Override
+    public String toString() {
+        return title + " (" + getStart() + " - " + getEnd() + ")";
+    }
 
 }
