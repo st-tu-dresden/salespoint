9619161de53bfc0e786a0f227a74b5d55843a3b2
hweisbach <hweisbach@eb53f5e8-9ec5-4bc0-8d13-06c033283886>
2012-12-11 16:58:53 +0000

    Adds new ctors to MetricMismatchException
    MetricMismatchException now optionally takes two Metrics to display which Metrics do not match.
    
    
    git-svn-id: svn://141.76.65.164/kp-salespoint@920 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/org/salespointframework/core/quantity/MetricMismatchException.java
+++ SalespointFramework/src/org/salespointframework/core/quantity/MetricMismatchException.java
@@ -16,4 +16,14 @@ public class MetricMismatchException extends RuntimeException
 	{
 		super(text);
 	}
+	
+	public MetricMismatchException(Metric m1, Metric m2)
+	{
+		super("Metric 1 (" + m1.getName() + ") does not match Metric 2 (" + m2.getName() +")" );
+	}
+	
+	public MetricMismatchException(String text, Metric m1, Metric m2)
+	{
+		super(text + "\n" + "Metric 1 (" + m1.getName() + ") does not match Metric 2 (" + m2.getName() +")" );
+	}
 }
