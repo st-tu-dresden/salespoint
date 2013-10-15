cb954c739d2c125bc1216cbcd227fdf9bd58f29d
Paul Henke <paul.henke@outlook.com>
2013-10-15 21:01:03 +0000

    removed usermanager references from Shop Singleton
    git-svn-id: svn://141.76.65.164/kp-salespoint@939 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/main/java/org/salespointframework/core/shop/Shop.java
+++ SalespointFramework/src/main/java/org/salespointframework/core/shop/Shop.java
@@ -4,9 +4,6 @@ import java.util.Objects;
 
 import org.salespointframework.core.time.DefaultTime;
 import org.salespointframework.core.time.Time;
-import org.salespointframework.core.user.TransientUserManager;
-import org.salespointframework.core.user.User;
-import org.salespointframework.core.user.UserManager;
 
 /**
  * Shop Singleton
@@ -21,7 +18,6 @@ public enum Shop {
 	INSTANCE;
 
 	private Time time = new DefaultTime();
-	private UserManager<? extends User> usermanager;
 
 	/**
 	 * Gets the global {@link Time}
@@ -40,35 +36,5 @@ public enum Shop {
 	public void setTime(Time time)
 	{
 		this.time = Objects.requireNonNull(time, "time must not be null");
-	}
-
-	/**
-	 * Gets the global {@link UserManager}
-	 * @return an UserManager instance
-	 */
-	public UserManager<? extends User> getUserManager()
-	{
-		return usermanager;
-	}
-
-	/**
-	 * Sets the global {@link UserManager}
-	 * @param userManager the UserManager to be set
-	 * @throws NullPointerException if userManager is null
-	 */
-	public void setUserManager(UserManager<? extends User> userManager)
-	{
-		this.usermanager = Objects.requireNonNull(userManager, "userManager must not be null");
-	}
-
-	
-	/**
-	 * Initializes the Shop with all <code>Transient</code> classes.
-	 */
-	public void initializeTransient()
-	{
-		Shop shop = Shop.INSTANCE;
-		
-		shop.setUserManager(new TransientUserManager());
 	}
 }
