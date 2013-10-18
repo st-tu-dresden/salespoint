7a4442f549f6dfd87748b3ea1c1494466816f40f
Paul Henke <paul.henke@outlook.com>
2013-10-18 13:15:18 +0000

    removed unused stuff add spring security broke Videoshop.java
    git-svn-id: svn://141.76.65.164/kp-salespoint@957 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- VideoShop/src/main/java/videoshop/VideoShop.java
+++ VideoShop/src/main/java/videoshop/VideoShop.java
@@ -84,4 +84,41 @@ public class VideoShop {
 			registry.addResourceHandler("/res/**").addResourceLocations("/resources/");
 		}
 	}
+
+	@EnableWebSecurity
+	@Configuration
+	static class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+	  @Override
+	  protected void registerAuthentication(AuthenticationManagerBuilder auth) {
+	    auth
+	      .inMemoryAuthentication()
+	        .withUser("user")  // #1
+	          .password("password")
+	          .roles("USER")
+	          .and()
+	        .withUser("admin") // #2
+	          .password("password")
+	          .roles("ADMIN","USER");
+	  }
+
+	  @Override
+	  public void configure(WebSecurity web) throws Exception {
+	    web
+	      .ignoring()
+	         .antMatchers("/resources/**"); // #3
+	  }
+
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http
+	      .authorizeUrls()
+	        .antMatchers("/signup","/about").permitAll() // #4
+	        .antMatchers("/admin/**").hasRole("ADMIN") // #6
+	        .anyRequest().authenticated() // 7
+	        .and()
+	    .formLogin()  // #8
+	        .loginUrl("/login") // #9
+	        .permitAll(); // #5
+	  }
+	}
 }
