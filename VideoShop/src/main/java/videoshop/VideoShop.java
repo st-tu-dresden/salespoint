d41e98f66452f4ded3c0f761ed54684729dd8f9a
Paul Henke <paul.henke@outlook.com>
2013-10-18 14:18:07 +0000

    added more spring-security libs
    git-svn-id: svn://141.76.65.164/kp-salespoint@960 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- VideoShop/src/main/java/videoshop/VideoShop.java
+++ VideoShop/src/main/java/videoshop/VideoShop.java
@@ -92,27 +92,27 @@ public class VideoShop {
 
 			return;
 			/*
-			auth.inMemoryAuthentication().withUser("user") // #1
-					.password("password").roles("USER").and().withUser("admin") // #2
-					.password("password").roles("ADMIN", "USER");
-					*/
+			 * auth.inMemoryAuthentication().withUser("user") // #1
+			 * .password("password").roles("USER").and().withUser("admin") // #2
+			 * .password("password").roles("ADMIN", "USER");
+			 */
 		}
 
-		//@Override
+		// @Override
 		public void configure_(WebSecurity web) throws Exception {
 			web.ignoring().antMatchers("/resources/**"); // #3
 		}
 
-		//@Override
-		protected void configure_(HttpSecurity http) throws Exception {
-			/*
-			http.authorizeUrls().antMatchers("/signup", "/about").permitAll() // #4
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().antMatchers("/", "/about").permitAll() // #4
 					.antMatchers("/admin/**").hasRole("ADMIN") // #6
 					.anyRequest().authenticated() // 7
+
 					.and().formLogin() // #8
-					.loginUrl("/login") // #9
+					.loginProcessingUrl("/login") // #9
 					.permitAll(); // #5
-				*/
+
 		}
 	}
 }
