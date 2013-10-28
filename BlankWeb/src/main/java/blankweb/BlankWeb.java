3735a80f7f424a31368682e6f31e40c239a3d8e9
Paul Henke <paul.henke@outlook.com>
2013-10-28 21:33:27 +0000

    addet init code
    git-svn-id: svn://141.76.65.164/kp-salespoint@1021 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- BlankWeb/src/main/java/blankweb/BlankWeb.java
+++ BlankWeb/src/main/java/blankweb/BlankWeb.java
@@ -1,63 +1,102 @@
 package blankweb;
 
+import java.util.List;
+
+import org.salespointframework.Salespoint;
+import org.salespointframework.core.useraccount.UserAccountDetailService;
+import org.salespointframework.core.useraccount.UserAccountManager;
+import org.salespointframework.spring.converter.JpaEntityConverter;
+import org.salespointframework.spring.converter.StringToRoleConverter;
+import org.salespointframework.spring.support.LoggedInUserAccountArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
 @EnableAutoConfiguration
-@Import({ BlankWeb.WebConfiguration.class })
+@Import({Salespoint.class, BlankWeb.WebConfiguration.class, BlankWeb.WebSecurityConfiguration.class })
 @ComponentScan
-//@ComponentScan(basePackages = "blankweb.controller")
 public class BlankWeb {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlankWeb.class, args);
 	}
-	
+
+
 	@Configuration
 	static class WebConfiguration extends WebMvcConfigurerAdapter {
 
-		// TODO Nötig??
-	    // messages for i18n
-	    // do not change the name of this method
-	    /*
-		@Bean
-	    public MessageSource messageSource() {
-	    	ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-	    	messageSource.setBasename("messages");
-	    	return messageSource;
-	    }
-	    */
-		
-	    // Salespoint5 goodies start
-		/*
-	   
-	    @Override
-	    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-	    	argumentResolvers.add(new org.salespointframework.web.spring.support.LoggedInUserArgumentResolver());
-	    	argumentResolvers.add(new org.salespointframework.web.spring.support.SalespointArgumentResolver());
-	    }
-	    
-	    @Override
-	    public void addInterceptors(InterceptorRegistry registry) {
-	    	registry.addInterceptor(new org.salespointframework.web.spring.support.LoggedInUserInterceptor());
-	    	registry.addInterceptor(new org.salespointframework.web.spring.support.CapabilitiesInterceptor());
-	    }
-	    
-	    @Override
-	    public void addFormatters(FormatterRegistry registry) {
-	    	registry.addConverterFactory(new org.salespointframework.web.spring.converter.StringToIdentifierConverterFactory());
-	    	registry.addConverter(new org.salespointframework.web.spring.converter.StringToCapabilityConverter());
-	    }
-	    */
-	    // Salespoint5 goodies end
+		@Autowired
+		private JpaEntityConverter entityConverter;
+
+		@Autowired
+		private UserAccountManager userAccountManager;
+
+		@Override
+		public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+			
+			argumentResolvers.add(new LoggedInUserAccountArgumentResolver(userAccountManager));
+		}
+
+		@Override
+		public void addFormatters(FormatterRegistry registry) {
+			registry.addConverter(entityConverter);
+			registry.addConverter(new StringToRoleConverter());
+		}
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Autowired
+		private UserAccountManager userAccountManager;
+
+		@Override
+		protected void registerAuthentication(AuthenticationManagerBuilder amBuilder) {
+
+			DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+			provider.setPasswordEncoder(userAccountManager.getPasswordEncoder());
+			provider.setUserDetailsService(new UserAccountDetailService(userAccountManager));
+
+			amBuilder.authenticationProvider(provider);
+
+			return;
+
+		}
+
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web.ignoring().antMatchers("/resources/**"); 
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+
+			http.csrf().disable();
+
+			http
+			.authorizeRequests().antMatchers("/**").permitAll()
+			.and()
+			.formLogin().loginProcessingUrl("/login")
+			.and()
+			.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+
+		}
 	}
 }
