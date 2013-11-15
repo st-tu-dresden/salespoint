a1cd6914914f6cbf013a980170b8c2be5e00a572
Paul Henke <paul.henke@outlook.com>
2013-11-15 17:21:56 +0000

    character encoding ergänzt, Umlaute und co werden jetzt korrekt von HtmlForms übertragen
    git-svn-id: svn://141.76.65.164/kp-salespoint@1043 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- Guestbook/src/main/java/guestbook/GBStart.java
+++ Guestbook/src/main/java/guestbook/GBStart.java
@@ -2,8 +2,11 @@ package guestbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
 
 // ‎(｡◕‿◕｡)
 // Der Einstiegspunkt unserer Applikation.
@@ -18,5 +21,13 @@ public class GBStart {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GBStart.class, args);
+	}
+	
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
 	}
 }
