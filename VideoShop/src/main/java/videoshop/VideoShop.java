c71167b026d5723d5ce50f0d773be5c93e05d240
Paul Henke <paul.henke@outlook.com>
2013-11-23 17:47:43 +0000

    Upgraded to Salespoint 5.3.1-SNAPSHOT. (Oliver Gierke)
    Removed obsolete web configuration as Salespoint will bootstrap it itself.
    
    git-svn-id: svn://141.76.65.164/kp-salespoint@1060 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- VideoShop/src/main/java/videoshop/VideoShop.java
+++ VideoShop/src/main/java/videoshop/VideoShop.java
@@ -1,11 +1,7 @@
 package videoshop;
 
-import java.util.List;
-
 import org.salespointframework.Salespoint;
-import org.salespointframework.core.useraccount.UserAccountManager;
-import org.salespointframework.spring.converter.JpaEntityConverter;
-import org.salespointframework.spring.converter.StringToRoleConverter;
+import org.salespointframework.SalespointWebConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -13,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -22,48 +17,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import videoshop.VideoShop.WebConfiguration;
-import videoshop.VideoShop.WebSecurityConfiguration;
 
 @Configuration
 @EnableAutoConfiguration
-@Import({ Salespoint.class, WebConfiguration.class, WebSecurityConfiguration.class })
+@Import({ Salespoint.class, SalespointWebConfiguration.class })
 @ComponentScan
 public class VideoShop {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideoShop.class, args);
-	}
-
-	@Bean
-	public CharacterEncodingFilter characterEncodingFilter() {
-		final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-		characterEncodingFilter.setEncoding("UTF-8");
-		characterEncodingFilter.setForceEncoding(true);
-		return characterEncodingFilter;
-	}
-
-	@Configuration
-	static class WebConfiguration extends WebMvcConfigurerAdapter {
-
-		@Autowired JpaEntityConverter entityConverter;
-		@Autowired UserAccountManager userAccountManager;
-		@Autowired List<HandlerMethodArgumentResolver> argumentResolvers;
-
-		@Override
-		public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-			argumentResolvers.addAll(this.argumentResolvers);
-		}
-
-		@Override
-		public void addFormatters(FormatterRegistry registry) {
-			registry.addConverter(entityConverter);
-			registry.addConverter(new StringToRoleConverter());
-		}
 	}
 
 	@Configuration
