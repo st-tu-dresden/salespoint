9bc5158853d2faf67ee76455a1095054d35a5d09
Paul Henke <paul.henke@outlook.com>
2013-12-02 17:59:57 +0000

    Upgraded to configuration simplifications in latest Salespoint - (Oliver Gierke)
    Removed default configuration pulled up into Salespoint. Upgraded to Spring Boot 0.5 M6 as it removes some configuration issues with regards to the database setup.
    
    git-svn-id: svn://141.76.65.164/kp-salespoint@1064 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- BlankWeb/src/main/java/blankweb/BlankWeb.java
+++ BlankWeb/src/main/java/blankweb/BlankWeb.java
@@ -1,25 +1,16 @@
 package blankweb;
 
-import org.salespointframework.Salespoint;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.salespointframework.SalespointSecurityConfiguration;
+import org.salespointframework.SalespointWebConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableAutoConfiguration
-@Import({ Salespoint.class })
+@Import({ SalespointWebConfiguration.class })
 @ComponentScan
 public class BlankWeb {
 
@@ -28,27 +19,8 @@ public class BlankWeb {
 	}
 
 	@Configuration
-	@EnableWebSecurity
 	@EnableGlobalMethodSecurity(prePostEnabled = true)
-	static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-		@Autowired AuthenticationProvider authenticationProvider;
-
-		@Override
-		protected void registerAuthentication(AuthenticationManagerBuilder amBuilder) {
-			amBuilder.authenticationProvider(authenticationProvider);
-		}
-
-		@Bean
-		@Override
-		public AuthenticationManager authenticationManagerBean() throws Exception {
-			return super.authenticationManagerBean();
-		}
-
-		@Override
-		public void configure(WebSecurity web) throws Exception {
-			web.ignoring().antMatchers("/resources/**");
-		}
+	static class WebSecurityConfiguration extends SalespointSecurityConfiguration {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
