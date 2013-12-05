5a40fbbd13fe41014452dcd413c743d3a49002b7
Paul Henke <paul.henke@outlook.com>
2013-12-05 11:55:45 +0000

    added comment
    git-svn-id: svn://141.76.65.164/kp-salespoint@1073 eb53f5e8-9ec5-4bc0-8d13-06c033283886

--- SalespointFramework/src/main/java/org/salespointframework/annotation/LoggedIn.java
+++ SalespointFramework/src/main/java/org/salespointframework/annotation/LoggedIn.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+// see LoggedInUserAccountArgumentResolver
+
 /**
  * 
  * @author Paul Henke
