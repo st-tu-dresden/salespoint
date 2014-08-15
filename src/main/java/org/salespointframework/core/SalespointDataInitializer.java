655fc8c07b125f9ac5817d3962774cffd2d381f3
Oliver Gierke <info@olivergierke.de>
2014-08-15 13:11:32 +0200

    #16 - Made Salespoint the top-level project.

--- src/main/java/org/salespointframework/core/SalespointDataInitializer.java
+++ src/main/java/org/salespointframework/core/SalespointDataInitializer.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.salespointframework.core;
 
 import java.util.List;
@@ -41,9 +26,26 @@ class SalespointDataInitializer {
 	public SalespointDataInitializer(List<DataInitializer> initializers) {
 
 		Assert.notNull(initializers, "DataIntializers must not be null!");
-		
+
 		for (DataInitializer initializer : initializers) {
 			initializer.initialize();
 		}
+	}
+
+	/**
+	 * Fallback {@link DataInitializer} to make sure we always have a t least one {@link DataInitializer} in the
+	 * APplication context so that the autowiring of {@link DataInitializer} into {@link SalespointDataInitializer} works.
+	 *
+	 * @author Oliver Gierke
+	 */
+	@Component
+	static class NoOpDataInitializer implements DataInitializer {
+
+		/* 
+		 * (non-Javadoc)
+		 * @see org.salespointframework.core.DataInitializer#initialize()
+		 */
+		@Override
+		public void initialize() {}
 	}
 }
