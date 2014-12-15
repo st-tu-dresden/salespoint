7961a511f0b16604782e473211ca1c14d4bf8429
Oliver Gierke <info@olivergierke.de>
2014-12-15 14:12:14 +0100

    #74 - Make sure we import the auto-configuration classes explicitly.
    We now explicitly import the auto-configuration classes as the component scanning activated on the annotation is applied to the annotated package not the package of the annotation.

--- src/test/java/example/EnableSalespointIntegrationTests.java
+++ src/test/java/example/EnableSalespointIntegrationTests.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package example;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -27,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.method.MethodSecurityMetadataSource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 /**
  * Integration tests for {@link EnableSalespoint}.
@@ -45,6 +32,8 @@ public class EnableSalespointIntegrationTests {
 	@Autowired ApplicationContext context;
 	@Autowired Catalog<Product> catalog;
 	@Autowired DummyRepository repository;
+	@Autowired Java8TimeDialect dialect;
+	@Autowired MethodSecurityMetadataSource securityMetadataSource;
 
 	@Test
 	public void componentsFromSalespointAndExampleAreAvailable() {
@@ -52,5 +41,11 @@ public class EnableSalespointIntegrationTests {
 		assertThat(context, is(notNullValue()));
 		assertThat(repository, is(notNullValue()));
 		assertThat(catalog, is(notNullValue()));
+
+		// Make sure the web configuration is included if not declared explicitly
+		assertThat(dialect, is(notNullValue()));
+
+		// Assert that @EnableGlobalMethodSecurity works
+		assertThat(securityMetadataSource, is(notNullValue()));
 	}
 }
