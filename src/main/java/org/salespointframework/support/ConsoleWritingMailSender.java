4f8a37ed77115f3c757f985dfc79da4c38b0d6c0
Oliver Gierke <info@olivergierke.de>
2015-11-18 14:30:01 +0100

    #126 - Improved JavaDoc of ConsoleWritingMailSender.
    The JavaDoc of ConsoleWritingMailSender now explicitly lists the steps necessary to use it in a Spring Boot application, including the bean definition, the necessary log settings as well as a pointer to instructions how to use a real MailSender implementation using the Spring Boot starter.

--- src/main/java/org/salespointframework/support/ConsoleWritingMailSender.java
+++ src/main/java/org/salespointframework/support/ConsoleWritingMailSender.java
@@ -11,8 +11,30 @@ import org.springframework.mail.SimpleMailMessage;
 /**
  * {@link MailSender} implementation writing the {@link SimpleMailMessage} to be sent into the logs. Should be used for
  * testing purposes only.
+ * <p>
+ * The component can be used by declaring a Spring bean definition for it, e.g. in a Spring Boot application like this:
+ * 
+ * <pre>
+ * &#64;Bean
+ * public MailSender mailSender() {
+ * 	return new ConsoleWritingMailSender();
+ * }
+ * </pre>
+ * 
+ * Make sure, you're setting the log level for Salespoint to INFO to actually see the sent messages. This can be
+ * achieved adding the following line to your {@code application.properties}.
+ * 
+ * <pre>
+ * logging.level.org.salespointframework.support = INFO
+ * </pre>
+ * 
+ * To use a real {@link MailSender} instead, include the {@code spring-boot-starter-mail} module in your Maven POM file
+ * and follow the instructions in the
+ * <a href="http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-email">reference
+ * documentation</a>.
  * 
  * @author Oliver Gierke
+ * @see http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-email
  */
 public class ConsoleWritingMailSender implements MailSender {
 
@@ -27,12 +49,12 @@ public class ConsoleWritingMailSender implements MailSender {
 		LOGGER.info(simpleMessage.toString());
 	}
 
-	/* 
+	/*
 	 * (non-Javadoc)
 	 * @see org.springframework.mail.MailSender#send(org.springframework.mail.SimpleMailMessage[])
 	 */
 	@Override
-	public void send(SimpleMailMessage[] simpleMessages) throws MailException {
+	public void send(SimpleMailMessage... simpleMessages) throws MailException {
 		Arrays.stream(simpleMessages).forEach(message -> send(message));
 	}
 }
