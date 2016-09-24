b9c22200d867eef9f7c3bf55bfb1d10d74f66422
Oliver Gierke <ogierke@pivotal.io>
2016-09-24 19:15:26 +0200

    #141 - Renamed SalespointDataInitializer to DataInitializerInvoker.
    The new class new better reflects the actual purpose of the class.

--- src/main/java/org/salespointframework/core/DataInitializerInvoker.java
+++ src/main/java/org/salespointframework/core/DataInitializerInvoker.java
@@ -2,7 +2,6 @@ package org.salespointframework.core;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
@@ -14,34 +13,31 @@ import org.springframework.util.Assert;
  * @author Oliver Gierke
  */
 @Component
-class SalespointDataInitializer {
+class DataInitializerInvoker {
 
 	/**
-	 * Creates a new {@link SalespointDataInitializer} with the given {@link DataInitializer} and triggers the
+	 * Creates a new {@link DataInitializerInvoker} with the given {@link DataInitializer} and triggers the
 	 * initialization.
 	 * 
 	 * @param initializers must not be {@literal null}.
 	 */
-	@Autowired
-	public SalespointDataInitializer(List<DataInitializer> initializers) {
+	public DataInitializerInvoker(List<DataInitializer> initializers) {
 
 		Assert.notNull(initializers, "DataIntializers must not be null!");
 
-		for (DataInitializer initializer : initializers) {
-			initializer.initialize();
-		}
+		initializers.forEach(DataInitializer::initialize);
 	}
 
 	/**
 	 * Fallback {@link DataInitializer} to make sure we always have a t least one {@link DataInitializer} in the
-	 * APplication context so that the autowiring of {@link DataInitializer} into {@link SalespointDataInitializer} works.
+	 * APplication context so that the autowiring of {@link DataInitializer} into {@link DataInitializerInvoker} works.
 	 *
 	 * @author Oliver Gierke
 	 */
 	@Component
 	static class NoOpDataInitializer implements DataInitializer {
 
-		/* 
+		/*
 		 * (non-Javadoc)
 		 * @see org.salespointframework.core.DataInitializer#initialize()
 		 */
