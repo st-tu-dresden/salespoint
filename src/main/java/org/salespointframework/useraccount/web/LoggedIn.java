3b9d488a4b4e813ea06fb0b930deb9495c663c07
Oliver Gierke <ogierke@pivotal.io>
2016-11-25 17:58:27 +0100

    #154 - Updated JavaDoc of @LoggedIn to describe all supported types.
    We now properly document that @LoggedIn can be used with a plain UserAccount parameter as well.

--- src/main/java/org/salespointframework/useraccount/web/LoggedIn.java
+++ src/main/java/org/salespointframework/useraccount/web/LoggedIn.java
@@ -6,12 +6,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.salespointframework.useraccount.UserAccount;
-
-// 
+import org.springframework.web.bind.ServletRequestBindingException;
 
 /**
  * Annotation to mark the method parameter with that shall get the {@link UserAccount} of the currently logged in user
- * injected. The method parameter needs to be of type {@code Optional<UserAccount>}.
+ * injected. The method parameter needs to be either of type {@code Optional<UserAccount>} (for methods that can support
+ * non-authenticated access) or {@link UserAccount}. In the latter case a {@link ServletRequestBindingException} will be
+ * thrown if no {@link UserAccount} could have been obtained in the first place.
  * 
  * @author Paul Henke
  * @author Oliver Gierke
