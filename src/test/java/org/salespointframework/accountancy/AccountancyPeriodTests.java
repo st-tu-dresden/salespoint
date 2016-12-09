0a9bd1bf3fd5a048733997f71a82208e3efc31ab
Oliver Gierke <ogierke@pivotal.io>
2016-12-09 15:04:49 +0100

    #157 - Removed console output from tests.

--- src/test/java/org/salespointframework/accountancy/AccountancyPeriodTests.java
+++ src/test/java/org/salespointframework/accountancy/AccountancyPeriodTests.java
@@ -37,59 +37,43 @@ public class AccountancyPeriodTests extends AbstractIntegrationTests {
 
 		Money oneEuro = Money.of(1, Currencies.EURO);
 
-		System.out.println("Creating AccountancyEntries: ");
-
 		for (int i = 0; i < 20; i++) {
 
 			ProductPaymentEntry p = new ProductPaymentEntry(orderIdentifier, account, oneEuro, "Rechnung nr. 3", Cash.CASH);
 			a.add(p);
 
-			System.out.println("Adding p " + p);
-
 			if (i == 5)
 				from = p.getDate().get();
 			if (i == 15)
 				to = p.getDate().get();
-
-			Thread.sleep(5);
 		}
 	}
 
 	@Test
 	public void periodSetTest() {
 
-		System.out.println("Getting entries from " + from + " to " + to);
-
 		Interval interval = Interval.from(from).to(to);
 
 		Map<Interval, Streamable<AccountancyEntry>> m = a.find(interval, Duration.ofMillis(200));
 		for (Entry<Interval, Streamable<AccountancyEntry>> e : m.entrySet()) {
-			System.out.println("ProductPaymentEntries for interval " + e.getKey());
-			for (AccountancyEntry p : e.getValue()) {
-				System.out.println("\t" + p);
-			}
+			for (AccountancyEntry p : e.getValue()) {}
 		}
 	}
 
 	@Test
 	public void singlePeriodTest() {
-		System.out.println("Getting entries from " + from + " to " + to);
 
 		Interval interval = Interval.from(from).to(to);
 
 		Map<Interval, Streamable<AccountancyEntry>> m = a.find(interval, interval.getDuration());
 		for (Entry<Interval, Streamable<AccountancyEntry>> e : m.entrySet()) {
-			System.out.println("AccountancyEntry for interval " + e.getKey());
-			for (AccountancyEntry p : e.getValue()) {
-				System.out.println("\t" + p);
-			}
+			for (AccountancyEntry p : e.getValue()) {}
 		}
 	}
 
 	@Test
 	public void periodMoneyTest() {
 		MonetaryAmount total;
-		System.out.println("Getting entries from " + from + " to " + to);
 
 		Interval interval = Interval.from(from).to(to);
 
@@ -97,14 +81,12 @@ public class AccountancyPeriodTests extends AbstractIntegrationTests {
 		for (Entry<Interval, Streamable<AccountancyEntry>> e : m.entrySet()) {
 			total = Currencies.ZERO_EURO;
 			for (AccountancyEntry p : e.getValue()) {
-				System.out.println("\t" + p.getValue());
 				total = total.add(p.getValue());
 			}
-			System.out.println("MonetaryAmount for interval " + e.getKey() + ": " + total);
-
 		}
-		System.out.println("Getting entries from " + from + " to " + to);
+
 		Map<Interval, MonetaryAmount> sales = a.salesVolume(interval, Duration.ofMillis(200));
+
 		for (Entry<Interval, MonetaryAmount> e : sales.entrySet()) {
 			System.out.println("MonetaryAmount for interval " + e.getKey() + ": " + e.getValue());
 		}
