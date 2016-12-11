4fb07176868360533e6bba85dbf3c81f72aa4a8b
Oliver Gierke <ogierke@pivotal.io>
2016-12-11 13:55:02 +0100

    #153 - Moved unit tests into the appropriate class.
    Moved tests for overlap detection of Interval from IntervalsUnitTests to IntervalUnitTests.

--- src/test/java/org/salespointframework/time/IntervalUnitTests.java
+++ src/test/java/org/salespointframework/time/IntervalUnitTests.java
@@ -1,6 +1,6 @@
 package org.salespointframework.time;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 import java.time.LocalDateTime;
@@ -90,5 +90,73 @@ public class IntervalUnitTests {
 
 		assertThat(first, is(not(second)));
 		assertThat(second, is(not(first)));
+	}
+
+	/**
+	 * @see #153
+	 */
+	@Test
+	public void detectsContainedDateTimes() {
+
+		LocalDateTime now = LocalDateTime.now();
+		LocalDateTime nowTomorrow = now.plusDays(1);
+		LocalDateTime nowYesterday = now.minusDays(1);
+
+		Interval interval = Interval.from(nowYesterday).to(nowTomorrow);
+
+		assertThat(interval.contains(now), is(true));
+		assertThat(interval.contains(nowTomorrow), is(true));
+		assertThat(interval.contains(nowYesterday), is(true));
+
+		assertThat(interval.contains(nowYesterday.minusDays(1)), is(false));
+		assertThat(interval.contains(nowTomorrow.plusDays(1)), is(false));
+	}
+
+	/**
+	 * @see #153
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void rejectsNullForContainsReference() {
+
+		LocalDateTime now = LocalDateTime.now();
+
+		Interval.from(now).to(now.plusDays(1)).contains(null);
+	}
+
+	/**
+	 * @see #153
+	 */
+	@Test
+	public void detectsOverlaps() {
+
+		LocalDateTime now = LocalDateTime.now();
+
+		Interval first = Interval.from(now.minusDays(1)).to(now.plusDays(2));
+		Interval second = Interval.from(now.minusDays(2)).to(now.plusDays(1));
+		Interval third = Interval.from(now).to(now.plusDays(1));
+		Interval fourth = Interval.from(now.minusDays(2)).to(now.minusDays(1));
+
+		// Partial
+		assertThat(first.overlaps(second), is(true));
+		assertThat(second.overlaps(first), is(true));
+
+		// Containment
+		assertThat(third.overlaps(first), is(true));
+		assertThat(first.overlaps(third), is(true));
+
+		// No overlap
+		assertThat(first.overlaps(fourth), is(false));
+		assertThat(fourth.overlaps(first), is(false));
+	}
+
+	/**
+	 * @see #153
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void rejectsNullForOverlapReference() {
+
+		LocalDateTime now = LocalDateTime.now();
+
+		Interval.from(now).to(now.plusDays(1)).overlaps(null);
 	}
 }
