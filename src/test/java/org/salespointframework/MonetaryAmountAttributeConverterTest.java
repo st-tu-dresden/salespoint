3a572494f043ef765620f943500411518b7761e9
Oliver Gierke <ogierke@pivotal.io>
2017-08-16 15:43:49 +0200

    #169 - Added license headers to source files.

--- src/test/java/org/salespointframework/MonetaryAmountAttributeConverterTest.java
+++ src/test/java/org/salespointframework/MonetaryAmountAttributeConverterTest.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.salespointframework;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
 
 import org.javamoney.moneta.Money;
 import org.junit.Test;
@@ -76,6 +93,17 @@ public class MonetaryAmountAttributeConverterTest {
 	public void convertsValuesWithTrailingZerosCorrectly() {
 
 		Money reference = Money.of(100.0, "EUR");
+
+		assertThat(converter.convertToEntityAttribute(converter.convertToDatabaseColumn(reference)), is(reference));
+	}
+
+	/**
+	 * @see #156
+	 */
+	@Test
+	public void convertsValuesWithTrailingZerosCorrectly2() {
+
+		Money reference = Money.of(new BigDecimal("100.0"), "EUR");
 
 		assertThat(converter.convertToEntityAttribute(converter.convertToDatabaseColumn(reference)), is(reference));
 	}
