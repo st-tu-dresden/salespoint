4159a07466c806b1997308f304f0e83e38cfb01d
Oliver Gierke <info@olivergierke.de>
2018-11-29 13:48:33 +0100

    #235 - Moved StringToRoleConverter to user account module.

--- src/main/java/org/salespointframework/useraccount/StringToRoleConverter.java
+++ src/main/java/org/salespointframework/useraccount/StringToRoleConverter.java
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.salespointframework.support;
+package org.salespointframework.useraccount;
 
-import org.salespointframework.useraccount.Role;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
