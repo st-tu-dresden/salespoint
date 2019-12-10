42f64a49ba8879a20f5dd90f2af0fe5d373c8a0c
Oliver Drotbohm <odrotbohm@icloud.com>
2019-12-10 10:55:11 +0100

    #292 - OrderManager.findAll(…) now allows sorting by properties of Order sub-types.
    We now redeclare OrderRepository.findAll(Pageable) and declare the find-all-query manually to make use of String based Sort binding as only that allows to create sort expressions that refer to properties of subtypes of Order.

---
+++ src/test/java/org/salespointframework/order/CustomOrder.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.salespointframework.order;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+
+import org.salespointframework.useraccount.UserAccount;
+
+/**
+ * Custom extension of {@link Order}.
+ *
+ * @author Oliver Drotbohm
+ */
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
+class CustomOrder extends Order {
+
+	private String customProperty;
+
+	@SuppressWarnings("unused")
+	private CustomOrder() {
+		super();
+	}
+
+	/**
+	 * Creates a new {@link CustomOrder} for the given {@link UserAccount} and custom property.
+	 *
+	 * @param userAccount must not be {@literal null}.
+	 * @param customProperty can be {@literal null}.
+	 */
+	public CustomOrder(UserAccount userAccount, String customProperty) {
+
+		super(userAccount);
+
+		this.customProperty = customProperty;
+	}
+}
