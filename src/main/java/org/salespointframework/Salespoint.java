631f0a27fa9f627d1121feab26cca5dc210ef034
Oliver Drotbohm <odrotbohm@icloud.com>
2022-12-02 14:00:48 +0100

    GH-417 - Remove obsolete EnvironmentPostProcessor.
    We switched to a different way of declaring default properties in 5ef57b25.

--- src/main/java/org/salespointframework/Salespoint.java
+++ src/main/java/org/salespointframework/Salespoint.java
@@ -15,23 +15,17 @@
  */
 package org.salespointframework;
 
-import java.util.HashMap;
-
 import org.salespointframework.inventory.LineItemFilter;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -60,32 +54,5 @@ public class Salespoint {
 	@Bean
 	public LineItemFilter inventoryLineItemFilter() {
 		return LineItemFilter.handleAll();
-	}
-
-	/**
-	 * Needed until https://github.com/spring-projects/spring-boot/issues/12194 and
-	 * https://github.com/spring-projects/spring-boot/issues/16747 are fixed.
-	 *
-	 * @author Oliver Gierke
-	 */
-	static class RequiredPropertiesPostProcessor implements EnvironmentPostProcessor {
-
-		/*
-		 * (non-Javadoc)
-		 * @see org.springframework.boot.env.EnvironmentPostProcessor#postProcessEnvironment(org.springframework.core.env.ConfigurableEnvironment, org.springframework.boot.SpringApplication)
-		 */
-		@Override
-		public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-
-			var sources = environment.getPropertySources();
-
-			var properties = new HashMap<String, Object>();
-			properties.put("spring.aop.proxy-target-class", false);
-			properties.put("spring.datasource.generate-unique-name", true);
-			properties.put("spring.jpa.open-in-view", true);
-			properties.put("spring.jpa.properties.hibernate.auto_quote_keyword", true);
-
-			sources.addFirst(new MapPropertySource("salespointDefaults", properties));
-		}
 	}
 }
