1842845ab659d31198c7bea78472a11d860cf82b
Armin Wolf <armin.wolf@mailbox.tu-dresden.de>
2022-12-09 17:56:53 +0100

    Use html notation for special characters
    Since javadoc comments are interpreted as html text,
    using special characters like brackets causes warnings.
    Fix this by using the html notation for such characters.
    
    Signed-off-by: Armin Wolf <armin.wolf@mailbox.tu-dresden.de>

--- src/main/java/org/salespointframework/order/OrderManagement.java
+++ src/main/java/org/salespointframework/order/OrderManagement.java
@@ -73,8 +73,9 @@ public interface OrderManagement<T extends Order> {
 	Streamable<T> findBy(OrderStatus orderStatus);
 
 	/**
-	 * Returns all {@link Order}s in between the {@link Interval}. So every entry with an time stamp <= to and >= from is
-	 * returned. If no {@link Order}s within the specified time span exist, an empty {@link Iterable} is returned.
+	 * Returns all {@link Order}s in between the {@link Interval}. So every entry with an time stamp &le;
+	 * and &ge; from is returned. If no {@link Order}s within the specified time span exist,
+	 * an empty {@link Iterable} is returned.
 	 *
 	 * @param interval The time interval to find {@link Order}s in, must not be {@literal null}.
 	 * @return a {@link Streamable} containing all {@link Order}s in the given {@link Interval}.
@@ -93,8 +94,8 @@ public interface OrderManagement<T extends Order> {
 
 	/**
 	 * Returns all {@link Order}s from the given {@link UserAccount} in between the dates {@code from} and {@code to},
-	 * including from and to. So every entry with an time stamp <= to and >= from is returned. If this user has no
-	 * {@link Order}s in this period, an empty {@link Iterable} is returned.
+	 * including from and to. So every entry with an time stamp &le; to and &ge; from is returned.
+	 * If this user has no {@link Order}s in this period, an empty {@link Iterable} is returned.
 	 *
 	 * @param userAccount The {@link UserAccount} whose {@link Order}s shall be returned, must not be {@literal null}.
 	 * @param interval The time interval to find {@link Order}s in, must not be {@literal null}.
